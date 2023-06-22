698c7ac01eec9c3a35a1193f13cf0af7c909e0b8
Tim Moreton <tmoreton@inspirecleanenergy.com>
2023-06-22 09:28:28 -0400

    fix footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,30 +1,28 @@
 import Container from './container'
 import { EXAMPLE_PATH } from '../lib/constants'
+import Newsletter from './newsletter'
 
 const Footer = () => {
   return (
-    <footer className="bg-neutral-50 border-t border-neutral-200">
-      <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
+    <footer>
+      <Newsletter />
+      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+        <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
+          <div className="pb-6">
+            <a href='/' className="text-sm leading-6 text-gray-600 hover:text-gray-900">
+              Home
             </a>
           </div>
-        </div>
-      </Container>
+          <div className="pb-6">
+            <a href='/blog' className="text-sm leading-6 text-gray-600 hover:text-gray-900">
+              Blog
+            </a>
+          </div>
+        </nav>
+        <p className="mt-10 text-center text-xs leading-5 text-gray-500">
+          &copy; 2023 Trckfi, Inc. All rights reserved.
+        </p>
+      </div>
     </footer>
   )
 }
---
+++ components/newsletter.tsx
@@ -0,0 +1,36 @@
+
+const Newsletter = () => {
+  return (
+    <div className="bg-white py-16 sm:py-24">
+      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="relative isolate overflow-hidden bg-pink-600 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
+          <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            Get notified when we’re launching.
+          </h2>
+          <form className="mx-auto mt-10 flex max-w-md gap-x-4">
+            <label htmlFor="email-address" className="sr-only">
+              Email address
+            </label>
+            <input
+              id="email-address"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              className="min-w-0 flex-auto rounded-md border-0 bg-white px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
+              placeholder="Enter your email"
+            />
+            <button
+              type="submit"
+              className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            >
+              Notify me
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Newsletter
