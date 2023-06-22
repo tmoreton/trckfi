f27d27eb3a6d7593c320e9bdfe998013a16b25ba
Tim Moreton <tmoreton@inspirecleanenergy.com>
2023-06-22 10:07:46 -0400

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
 import Header from '../components/header'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
-      <Component {...pageProps} />
+      <Component {...pageProps}/>
+      <Analytics />
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import Hero from '../components/hero'
 import Layout from '../components/layout'
 import { getAllPosts } from '../lib/api'
 import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
 import Post from '../interfaces/post'
 
 type Props = {
@@ -17,7 +16,7 @@ export default function Index({ allPosts }: Props) {
     <>
       <Layout>
         <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+          <title>Trckfi - Newsletter</title>
         </Head>
         <Container>
           <Hero />
