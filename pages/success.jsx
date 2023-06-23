0c375e6c67b73367a3263d809c224460bf28d3b1
Tim Moreton <tmoreton@inspirecleanenergy.com>
2023-06-23 12:27:22 -0400

    account for not compelting checkout

--- pages/success.jsx
+++ pages/success.jsx
@@ -1,10 +1,9 @@
 import Congrats from '../components/congrats'
 import Stripe from 'stripe';
-import { useSession, signIn } from "next-auth/react"
+import { signIn } from "next-auth/react"
 import prisma from '../lib/prisma';
 
 export default function ({ data }) {
-  const { data: session } = useSession()
   if (!data) {
     return (
       <button onClick={() => signIn()} className="rounded-md bg-pink-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pink-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">
@@ -23,7 +22,9 @@ export async function getServerSideProps(context) {
       apiVersion: '2022-11-15',
     });
     // Fetch data from external API
-    const { customer }  = await stripe.checkout.sessions.retrieve(session_id);
+    const { customer }  = await stripe.checkout.sessions.retrieve(session_id)
+    if(!customer) return { props: {} }
+
     const data = await stripe.customers.retrieve(customer)
     const { id, email, name, phone } = data
 
@@ -41,4 +42,5 @@ export async function getServerSideProps(context) {
 
     return { props: { data: user } }
   }
+  return { props: {} }
 }
