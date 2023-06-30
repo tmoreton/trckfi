2ceba13c67dc1888c1babbbf500df592a37dfe34
Tim Moreton <tmoreton@inspirecleanenergy.com>
2023-06-30 17:24:24 -0400

    change border to hr

--- emails/monthly_summary.tsx
+++ emails/monthly_summary.tsx
@@ -155,7 +155,7 @@ export default function ({ month, thisMonth, categories, thisMonthTotal, lastMon
                         </Text>
                       </Column>
                       <Column className="w-40" align="left">
-                        <Text className="text-[#666666] text-[8px] my-0 ml-8">
+                        <Text className="text-[#666666] text-[7px] my-0 ml-8">
                           {item.primary_category.split('_').join(' ')}
                         </Text>
                       </Column>
