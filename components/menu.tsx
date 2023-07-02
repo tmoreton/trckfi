371a9a5d57612413a615a6ffba99ea7717791a70
Tim Moreton <tmoreton@inspirecleanenergy.com>
2023-07-01 21:40:46 -0400

    add mobile menu

--- components/menu.tsx
+++ components/menu.tsx
@@ -31,20 +31,21 @@ export default function () {
       
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div className={show && 'hidden'}>
-          <div className="text-sm lg:flex-grow">
+          <div className="lg:flex-grow">
             {navigation.map((item) => (
               <Link href={item.href} key={item.name} className={currentRoute === item.href ? "block mt-4 lg:inline-block lg:mt-0 mr-4 font-bold text-pink-600" : "block mt-4 lg:inline-block lg:mt-0 mr-4 text-gray-900"}>
                 {item.name}
               </Link>
             ))}
+            <div className="sm:hidden">
+              <LoginBtn />
+            </div>
           </div>
         </div>
       </div>
 
       <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-          <LoginBtn />
-        </a>
+        <LoginBtn />
       </div>
     </nav>
   )
