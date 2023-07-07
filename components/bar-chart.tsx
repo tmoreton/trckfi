e906056e8a1c7683fbca1361f45ea6e977affe60
Tim Moreton <tmoreton@inspirecleanenergy.com>
2023-07-06 20:17:58 -0400

    fix xharts

--- components/bar-chart.tsx
+++ components/bar-chart.tsx
@@ -41,26 +41,40 @@ export const options = {
 }
 
 export default function ({ monthlyIncomeData, monthlyExpenseData }) {
-  if (monthlyIncomeData.length <= 0 || monthlyExpenseData.length <= 0) return null
+  if (monthlyIncomeData.length <= 0 && monthlyExpenseData.length <= 0) return null
 
   const monthlyLabel = monthlyExpenseData.map(a => a.dt_string)
   const monthlySum = monthlyIncomeData.map(a => Math.abs(a._sum.amount))
   const monthlyExpenseSum = monthlyExpenseData.map(a => Math.abs(a._sum.amount))
-
-  const data = {
-    labels: monthlyLabel,
-    datasets: [
-      {
-        label: 'Income',
-        data: monthlySum,
-        backgroundColor: '#009c7b'
-      }, {
-        label: 'Expense',
-        data: monthlyExpenseSum,
-        backgroundColor: '#ff6384'
-      },
-    ],
+  let data = {}
+  
+  if(monthlyIncomeData.length <= 0){
+    data = {
+      labels: monthlyLabel,
+      datasets: [
+        {
+          label: 'Expenses',
+          data: monthlyExpenseSum,
+          backgroundColor: '#ff6384'
+        },
+      ],
+    }
+  } else {
+    data = {
+      labels: monthlyLabel,
+      datasets: [
+        {
+          label: 'Income',
+          data: monthlySum,
+          backgroundColor: '#009c7b'
+        }, {
+          label: 'Expenses',
+          data: monthlyExpenseSum,
+          backgroundColor: '#ff6384'
+        },
+      ],
+    }
   }
-  console.log(data)
+
   return <div className="sm:w-2/3 w-100 mx-auto mt-8 p-0 sm:pl-28"><Bar options={options} data={data} /></div>
 }
