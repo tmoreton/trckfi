da2901a1f29578192ad22112d1ab55e12adddbb9
Tim Moreton <tmoreton@inspirecleanenergy.com>
2023-07-07 03:29:24 -0400

    remove ai

--- pages/api/open_ai.tsx
+++ pages/api/open_ai.tsx
@@ -1,24 +1,24 @@
-import { Configuration, OpenAIApi } from 'openai-edge'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
-export const runtime = 'edge'
+// import { Configuration, OpenAIApi } from 'openai-edge'
+// import { OpenAIStream, StreamingTextResponse } from 'ai'
+// export const runtime = 'edge'
 
-const config = new Configuration({
-  apiKey: process.env.OPEN_API_KEY
-})
+// const config = new Configuration({
+//   apiKey: process.env.OPEN_API_KEY
+// })
 
-const openai = new OpenAIApi(config)
+// const openai = new OpenAIApi(config)
 
 export default async (req, res) => {
-  const { messages } = await req.json()
+  // const { messages } = await req.json()
 
-  const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages
-  })
+  // const response = await openai.createChatCompletion({
+  //   model: 'gpt-3.5-turbo',
+  //   stream: true,
+  //   messages
+  // })
 
-  const stream = OpenAIStream(response)
+  // const stream = OpenAIStream(response)
 
-  // Respond with the stream
-  return new StreamingTextResponse(stream)
+  // // Respond with the stream
+  // return new StreamingTextResponse(stream)
 }
