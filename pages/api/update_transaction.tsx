201c4c37fcdac8a7fe233016d5d9b51dfe3e2c66
Tim Moreton <tmoreton@Tims-MacBook-Pro.local>
2023-07-12 18:51:30 -0400

    updated the transactions

--- pages/api/update_transaction.tsx
+++ pages/api/update_transaction.tsx
@@ -1,23 +1,31 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 import prisma from '../../lib/prisma';
 import { snakeCase } from "snake-case";
+import { DateTime } from "luxon"
 
 export default async (req, res) => {
   const { transaction, ids } = JSON.parse(req.body)
   if (!transaction) return res.status(500).json({ error: 'No Transaction' })
-  const { id, name, unified, primary_category, detailed_category, amount, notes } = transaction
+  const { id, name, unified, primary_category, detailed_category, amount, notes, date } = transaction
   try {
     if(ids.length > 0){
       ids.forEach( async (i) => {
         const item = await prisma.transactions.findUnique({
           where: { id: i }
         })
+        
         let data = {}
         if (name) data['name'] = name
         if (notes) data['notes'] = notes
         if (unified && unified !== '1f50d') data['unified'] = unified
         if (primary_category) data['primary_category'] = snakeCase(primary_category).toUpperCase()
         if (detailed_category) data['detailed_category'] = snakeCase(detailed_category).toUpperCase()
+        if (date) {
+          data['date'] = date
+          data['authorized_date'] = new Date(date)
+          data['month_year'] = date.substring(0,7)
+          data['week_year'] = `${date.substring(0,4)}-${DateTime.fromISO(date).weekNumber}`
+        }
         await prisma.transactions.updateMany({
           where: { name: item.name },
           data
@@ -32,11 +40,14 @@ export default async (req, res) => {
           detailed_category: snakeCase(detailed_category).toUpperCase(),
           name,
           unified,
-          notes
+          notes,
+          date,
+          authorized_date: new Date(date),
+          month_year: date.substring(0,7),
+          week_year: `${date.substring(0,4)}-${DateTime.fromISO(date).weekNumber}`
         }
       })
     }
-
     return res.status(200).json({ status: 'OK' })
   } catch (error) {
     console.error(error)
