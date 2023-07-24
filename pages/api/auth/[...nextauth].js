daae3288b6b5f449b1c7e449332fa6247ab987c2
Tim Moreton <tmoreton89@gmail.com>
2023-07-24 15:19:42 -0400

    change maxage session to 24 hrs

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -60,6 +60,30 @@ export const authOptions = {
   },
   adapter: PrismaAdapter(prisma),
   secret: process.env.JWT_SECRET,
+  session: {
+    // Choose how you want to save the user session.
+    // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
+    // If you use an `adapter` however, we default it to `"database"` instead.
+    // You can still force a JWT session by explicitly defining `"jwt"`.
+    // When using `"database"`, the session cookie will only contain a `sessionToken` value,
+    // which is used to look up the session in the database.
+    strategy: "database",
+  
+    // Seconds - How long until an idle session expires and is no longer valid.
+    // maxAge: 30 * 24 * 60 * 60, // 30 days
+    maxAge: 24 * 60 * 60, // 24 hours
+
+    // Seconds - Throttle how frequently to write to database to extend a session.
+    // Use it to limit write operations. Set to 0 to always update the database.
+    // Note: This option is ignored if using JSON Web Tokens
+    updateAge: 24 * 60 * 60, // 24 hours
+    
+    // The session token is usually either a random UUID or string, however if you
+    // need a more customized session token string, you can define your own generate function.
+    generateSessionToken: () => {
+      return randomUUID?.() ?? randomBytes(32).toString("hex")
+    }
+  },
   callbacks: {
     async session(session, user) {
       return session
