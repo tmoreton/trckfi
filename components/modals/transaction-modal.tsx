378fdd55fdfcff4547d5c876d7563a9909452e17
Tim Moreton <tmoreton89@gmail.com>
2023-07-29 18:13:06 -0400

    add ignore

--- components/modals/transaction-modal.tsx
+++ components/modals/transaction-modal.tsx
@@ -180,6 +180,7 @@ export default function ({ item, setEdit, getDashboard, showError, selected, use
                                 id="account_id"
                                 name="account_id"
                                 onChange={handleChange}
+                                // @ts-ignore
                                 defaultValue={transaction?.account_id}
                                 className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm order-0 border-y border-x border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-pink-600 sm:text-sm sm:leading-6"
                               >
