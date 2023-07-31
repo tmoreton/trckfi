e623c8720815f3ba6042eb61ddaecdc3bcc92288
Tim Moreton <tmoreton89@gmail.com>
2023-07-31 10:56:25 -0400

    add rules

--- pages/api/sync_accounts.tsx
+++ pages/api/sync_accounts.tsx
@@ -55,7 +55,7 @@ export default async (req, res) => {
         let { id, type } = plaid[p]?.accounts.find(a => a.account_id === added[i].account_id)
         let detailed_category = added[i].personal_finance_category.detailed.replace(`${added[i].personal_finance_category.primary}_`, '')
         let { amount } = formatAmount(type, added[i].amount)
-        let { ruleset } = rules.find(r => r.identifier === added[i].name.toUpperCase())
+        let rule = rules.find(r => r.identifier === added[i].name.toUpperCase())
         await prisma.transactions.upsert({
           where: { 
             transaction_id: added[i].transaction_id 
@@ -67,12 +67,12 @@ export default async (req, res) => {
             amount: amount,
             authorized_date: new Date(added[i].date),
             date: added[i].date,
-            name: ruleset?.name || added[i].name,
+            name: rule?.ruleset?.name || added[i].name,
             merchant_name: added[i].merchant_name,
             category: added[i].category,
-            detailed_category: ruleset?.detailed_category || detailed_category,
+            detailed_category: rule?.ruleset?.detailed_category || detailed_category,
             unified: icons[detailed_category],
-            primary_category: ruleset?.primary_category || added[i].personal_finance_category.primary,
+            primary_category: rule?.ruleset?.primary_category || added[i].personal_finance_category.primary,
             // @ts-ignore
             location: added[i].location,
             user_id: user.id,
