1fc0a9e6b63381bc0b47269a49ffddd39ddbde84
Tim Moreton <tmoreton89@gmail.com>
2023-08-02 11:45:40 -0400

    update default

--- pages/auth/email-signin.tsx
+++ pages/auth/email-signin.tsx
@@ -1,52 +1,58 @@
 import type { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { getCsrfToken } from "next-auth/react"
+import Head from 'next/head'
 import Icon from '../../components/icon';
 import Link from 'next/link'
 
 export default function SignIn({ csrfToken, base_url }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-20 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <Icon />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account
-        </h2>
-      </div>
-      <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" method="post" action={`/api/auth/signin/email?callbackUrl=${base_url}/success`}>
-        {/* <form className="space-y-6" method="post" action={`/api/auth/signin/email`}> */}
-          <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-          <div>
-            <label htmlFor="email" className="block text-sm font-small leading-6 text-pink-600">
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-pink-600 peer" 
-                />
+    <>
+      <Head>
+        <title>Trckfi - Login</title>
+      </Head> 
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-20 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <Icon />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+        <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" method="post" action={`/api/auth/signin/email?callbackUrl=${base_url}/success`}>
+          {/* <form className="space-y-6" method="post" action={`/api/auth/signin/email`}> */}
+            <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+            <div>
+              <label htmlFor="email" className="block text-sm font-small leading-6 text-pink-600">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-pink-600 peer" 
+                  />
+              </div>
             </div>
-          </div>
-          <button
-            type="submit"
-            className="flex text-center justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600 mx-auto"
-          >
-            Sign in
-          </button>
-        </form>
+            <button
+              type="submit"
+              className="flex text-center justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600 mx-auto"
+            >
+              Sign in
+            </button>
+          </form>
 
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Not a member?{' '}
-          <Link href="/getting-started" className="font-semibold leading-6 text-pink-600 hover:text-pink-500">
-            Get started
-          </Link>
-        </p>
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <Link href="/getting-started" className="font-semibold leading-6 text-pink-600 hover:text-pink-500">
+              Get started
+            </Link>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- pages/auth/verify-request.tsx
+++ pages/auth/verify-request.tsx
@@ -1,22 +1,28 @@
 import Icon from '../../components/icon';
+import Head from 'next/head'
 
 export default function () {
   return (
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <Icon />
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Check Your Email</h1>
-        <p className="mt-6 text-base leading-7 text-gray-600">A magic sign-in link has been sent to your email address.</p>
-        <p className="text-xs text-gray-400">
-          PS. Check your spam folder since it's your first email from us.
-        </p>
-        <p className="mt-6 text-base leading-7 text-gray-600">Having trouble logging in?</p>
-        <div className="mt-2 flex items-center justify-center gap-x-6">
-          <a href="mailto:tim@trckfi.com" className="text-sm font-semibold text-gray-900">
-            Contact support <span aria-hidden="true">&rarr;</span>
-          </a>
+    <>
+      <Head>
+        <title>Trckfi - Verify</title>
+      </Head> 
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <Icon />
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Check Your Email</h1>
+          <p className="mt-6 text-base leading-7 text-gray-600">A magic sign-in link has been sent to your email address.</p>
+          <p className="text-xs text-gray-400">
+            PS. Check your spam folder since it's your first email from us.
+          </p>
+          <p className="mt-6 text-base leading-7 text-gray-600">Having trouble logging in?</p>
+          <div className="mt-2 flex items-center justify-center gap-x-6">
+            <a href="mailto:tim@trckfi.com" className="text-sm font-semibold text-gray-900">
+              Contact support <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   )
 }
