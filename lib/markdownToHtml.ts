7048b7769f41f0a331656654ea124f6338bcca86
Tim Moreton <tmoreton89@gmail.com>
2023-08-02 15:17:27 -0400

    fix smooth scroll

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,10 @@
 import { remark } from 'remark'
 import html from 'remark-html'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeStringify from 'rehype-stringify'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
+  const result = await remark().use(html, { sanitize: false }).process(markdown)
   return result.toString()
 }
