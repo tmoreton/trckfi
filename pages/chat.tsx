4904e5eb3a602d69b3c0dbb3fe2a5cc3aa2e8dfc
Tim Moreton <tmoreton89@gmail.com>
2023-08-05 18:45:36 -0400

    update prompt

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -11,7 +11,6 @@ export default function ({ showError }) {
 
   useEffect(() => {
     if(session?.user && !initialInput) startPrompt()
-    startPrompt()
   }, [])
 
   const startPrompt = async () => {
