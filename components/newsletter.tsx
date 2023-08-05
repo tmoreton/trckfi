eb1e6d905e8fc76cda0ec389af0a4b50f37a88d3
Tim Moreton <tmoreton89@gmail.com>
2023-08-05 09:23:22 -0400

    update homepage

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -51,7 +51,7 @@ const Newsletter = ({ showError }) => {
               type="submit"
               className="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
             >
-              { subscribed ? "Subscribed!  🎉" : "Notify me"}
+              { subscribed ? "Success!  🎉" : "Join Waitlist"}
             </button>
             {subscribed && <ConfettiExplosion />}
           </form>
