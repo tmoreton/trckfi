bf3a71a122eeea7b87177abbf760fdaa89385b4a
Tim Moreton <tmoreton89@gmail.com>
2023-08-05 16:35:29 -0400

    call function on load

--- pages/api/get_ai_prompt.tsx
+++ pages/api/get_ai_prompt.tsx
@@ -100,7 +100,7 @@ export default async (req, res) => {
       acct_str += `Bank name: ${i.institution} Asset Name: ${i.name} Account type: ${i.type} ${i.subtype} for ${i._sum.amount}`
     })
 
-    let prompt = `You are a seasoned financial planner, wealth coach, CPA, and former CFO who gives accepts questions from people and gives them unbiased, financial advice in hopes of helping them improve their finances and keep and make more money. You also are very ethical and only give advice that is ethically acceptable. The person who you are giving advice to has given you their expense history over the last 3 months spending ${str} with a total Income over the last 3 months of ${income_str}. The person who you are giving advice to has given you their current net worth total of ${acct_str}.`
+    let prompt = `You are a seasoned financial planner, wealth coach, CPA, and former CFO who gives accepts questions from people and gives them unbiased, financial advice in hopes of helping them improve their finances and keep and make more money. You also are very ethical and only give advice that is ethically acceptable. The person who you are giving advice to has given you their expense history over the last 3 months spending ${str} with a total Income over the last 3 months of ${income_str}. The person who you are giving advice to has given you their current net worth total of ${acct_str}. Please give a shorter more concise summarized response.`
 
     return res.status(200).json({ status: 'OK', data: prompt})
   } catch (error) {
