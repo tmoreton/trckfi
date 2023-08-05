894dd1d7a0330212c603f1e492a1a9f2256e4a3b
Tim Moreton <tmoreton89@gmail.com>
2023-08-05 16:21:34 -0400

    call function on load

--- pages/api/get_ai_prompt.tsx
+++ pages/api/get_ai_prompt.tsx
@@ -9,7 +9,8 @@ export default async (req, res) => {
   const endDate = DateTime.now().minus({ months: 3 }).startOf('month').toISO()
 
   try {
-    const income = await prisma.transactions.aggregate({
+    const income = await prisma.transactions.groupBy({
+      by: ['name', 'primary_category', 'detailed_category'],
       where: {
         OR: [
           { user_id: user.id },
@@ -33,6 +34,10 @@ export default async (req, res) => {
       _sum: {
         amount: true,
       },
+    })
+    let income_str = ''
+    income.map(i => {
+      income_str += `Name: ${i.name} Category: ${i.detailed_category} with ${i._count.amount} for ${i._sum.amount}`
     })
 
     const expenses = await prisma.transactions.groupBy({
@@ -68,6 +73,11 @@ export default async (req, res) => {
       take: 100
     })
 
+    let str = ''
+    expenses.map(i => {
+      str += `Name: ${i.name} Category: ${i.detailed_category} with ${i._count.amount} for ${i._sum.amount}`
+    })
+
     const accts = await prisma.accounts.groupBy({
       by: ['institution', 'type', 'subtype'],
       where: {
@@ -90,11 +100,7 @@ export default async (req, res) => {
       acct_str += `Bank name: ${i.institution} Account type: ${i.type} ${i.subtype} for ${i._sum.amount}`
     })
 
-    let str = ''
-    expenses.map(i => {
-      str += `Name: ${i.name} Category: ${i.detailed_category} with ${i._count.amount} for ${i._sum.amount}`
-    })
-    let prompt = `You are a seasoned financial planner, wealth coach, CPA, and former CFO who gives accepts questions from people and gives them unbiased, financial advice in hopes of helping them improve their finances and keep and make more money. You also are very ethical and only give advice that is ethically acceptable. The person who you are giving advice to has given you their expense history over the last 3 months spending ${str} with a total Income over the last 3 months of ${income._sum.amount}. The person who you are giving advice to has given you their current net worth total of ${acct_str}.`
+    let prompt = `You are a seasoned financial planner, wealth coach, CPA, and former CFO who gives accepts questions from people and gives them unbiased, financial advice in hopes of helping them improve their finances and keep and make more money. You also are very ethical and only give advice that is ethically acceptable. The person who you are giving advice to has given you their expense history over the last 3 months spending ${str} with a total Income over the last 3 months of ${income_str}. The person who you are giving advice to has given you their current net worth total of ${acct_str}.`
 
     return res.status(200).json({ status: 'OK', data: prompt})
   } catch (error) {
