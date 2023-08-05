24457887c6b4e68cf4f28082a75f5451c6a26720
Tim Moreton <tmoreton89@gmail.com>
2023-08-05 11:58:10 -0400

    fix

--- pages/api/get_ai_prompt.tsx
+++ pages/api/get_ai_prompt.tsx
@@ -6,7 +6,7 @@ export default async (req, res) => {
   const { user } = req.body
   if (!user) return res.status(500)
   const startDate = DateTime.now().toISO()
-  const endDate = DateTime.now().minus({ months: 3 }).startOf('month').toISO()
+  const endDate = DateTime.now().minus({ months: 1 }).startOf('month').toISO()
   let prompt = `You are a seasoned financial planner, wealth coach, CPA, and former CFO who gives accepts questions from people and gives them unbiased, financial advice in hopes of helping them improve their finances and keep and make more money. You also are very ethical and only give advice that is ethically acceptable. The person who you are giving advice to has given you their expense history over the last 3 months spending `
 
   try {
@@ -33,7 +33,7 @@ export default async (req, res) => {
 
     let str = ''
     detailedCategories.map(i => {
-      str += `Transaction Name: ${i.name} Category: ${i.detailed_category} and Amount: ${i._sum.amount}`
+      str += `Name: ${i.name} Category: ${i.detailed_category} ${i._sum.amount}`
     })
     let final = prompt+str
     return res.status(200).json({ status: 'OK', data: final})
