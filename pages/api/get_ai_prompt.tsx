34ead24dac2e17f4bc4f069bc56197b18f32d1cf
Tim Moreton <tmoreton89@gmail.com>
2023-08-05 12:30:44 -0400

    fix

--- pages/api/get_ai_prompt.tsx
+++ pages/api/get_ai_prompt.tsx
@@ -9,7 +9,33 @@ export default async (req, res) => {
   const endDate = DateTime.now().minus({ months: 3 }).startOf('month').toISO()
 
   try {
-    const detailedCategories = await prisma.transactions.groupBy({
+    const income = await prisma.transactions.aggregate({
+      where: {
+        OR: [
+          { user_id: user.id },
+          { user_id: user?.linked_user_id },
+        ],
+        active: true,
+        authorized_date: {
+          lte: startDate,
+          gte: endDate
+        },
+        amount: {
+          gte: 0,
+        },
+        NOT: [
+          { detailed_category: 'CREDIT_CARD_PAYMENT' },
+        ],
+      },
+      _count: {
+        amount: true,
+      },
+      _sum: {
+        amount: true,
+      },
+    })
+
+    const expenses = await prisma.transactions.groupBy({
       by: ['name', 'primary_category', 'detailed_category'],
       where: {
         OR: [
@@ -21,6 +47,12 @@ export default async (req, res) => {
           lte: startDate,
           gte: endDate
         },
+        amount: {
+          lte: 0,
+        },
+        NOT: [
+          { detailed_category: 'CREDIT_CARD_PAYMENT' },
+        ],
       },
       _count: {
         amount: true,
@@ -33,15 +65,14 @@ export default async (req, res) => {
           amount: 'asc',
         },
       },
-      take: 50
+      take: 100
     })
 
     let str = ''
-    detailedCategories.map(i => {
+    expenses.map(i => {
       str += `Name: ${i.name} Category: ${i.detailed_category} with ${i._count.amount} for ${i._sum.amount}`
     })
-    console.log(str)
-    let prompt = `You are a seasoned financial planner, wealth coach, CPA, and former CFO who gives accepts questions from people and gives them unbiased, financial advice in hopes of helping them improve their finances and keep and make more money. You also are very ethical and only give advice that is ethically acceptable. The person who you are giving advice to has given you their expense history over the last 3 months spending ${str}`
+    let prompt = `You are a seasoned financial planner, wealth coach, CPA, and former CFO who gives accepts questions from people and gives them unbiased, financial advice in hopes of helping them improve their finances and keep and make more money. You also are very ethical and only give advice that is ethically acceptable. The person who you are giving advice to has given you their expense history over the last 3 months spending ${str} and income of ${income._sum.amount} `
 
     return res.status(200).json({ status: 'OK', data: prompt})
   } catch (error) {
