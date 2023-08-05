95c4d1c9cbbc3368c08b0a68fa4288c37d6ce83e
Tim Moreton <tmoreton89@gmail.com>
2023-08-05 12:07:51 -0400

    fix

--- pages/api/get_ai_prompt.tsx
+++ pages/api/get_ai_prompt.tsx
@@ -5,9 +5,8 @@ import { DateTime } from "luxon"
 export default async (req, res) => {
   const { user } = req.body
   if (!user) return res.status(500)
-  const startDate = DateTime.now().toISO()
-  const endDate = DateTime.now().minus({ months: 1 }).startOf('month').toISO()
-  let prompt = `You are a seasoned financial planner, wealth coach, CPA, and former CFO who gives accepts questions from people and gives them unbiased, financial advice in hopes of helping them improve their finances and keep and make more money. You also are very ethical and only give advice that is ethically acceptable. The person who you are giving advice to has given you their expense history over the last 3 months spending `
+  const startDate = DateTime.now().startOf('month').toISO()
+  const endDate = DateTime.now().minus({ months: 3 }).startOf('month').toISO()
 
   try {
     const detailedCategories = await prisma.transactions.groupBy({
@@ -35,8 +34,9 @@ export default async (req, res) => {
     detailedCategories.map(i => {
       str += `Name: ${i.name} Category: ${i.detailed_category} ${i._sum.amount}`
     })
-    let final = prompt+str
-    return res.status(200).json({ status: 'OK', data: final})
+    let prompt = `You are a seasoned financial planner, wealth coach, CPA, and former CFO who gives accepts questions from people and gives them unbiased, financial advice in hopes of helping them improve their finances and keep and make more money. You also are very ethical and only give advice that is ethically acceptable. The person who you are giving advice to has given you their expense history over the last 3 months spending ${JSON.stringify(detailedCategories)}`
+
+    return res.status(200).json({ status: 'OK', data: prompt})
   } catch (error) {
     console.error(error)
     return res.status(500).json({ error: error.message || error.toString() })
