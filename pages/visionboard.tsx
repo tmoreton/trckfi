04b41eb0097f585e8d08c79ef58c8ff05a86a42b
Tim Moreton <tmoreton89@gmail.com>
2023-08-06 12:23:29 -0400

    remove old files

--- pages/visionboard.tsx
+++ pages/visionboard.tsx
@@ -1,18 +1,10 @@
-import { useState, useEffect } from 'react'
 import dynamic from 'next/dynamic' 
 import Container from '../components/container'
 import DashboardLayout from '../components/dashboard-layout'
 import Head from 'next/head'
-import DrawingTool from '../components/drawing-tool'
-import Board from '../components/board'
 const Editor = dynamic(() => import('../components/editor'), { ssr: false })
 
 export default function ({ showError }) {
-  const [updateEmail, setUpdateEmail] = useState('')
-
-  useEffect(() => {
-  }, [])
-
   return (
     <DashboardLayout>
       <Head>
