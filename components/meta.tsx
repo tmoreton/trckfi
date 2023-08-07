c8fb15827bb8695e087ce84ca18fd46ead0fc280
Tim Moreton <tmoreton89@gmail.com>
2023-08-07 10:26:47 -0400

    udpate meta tags

--- components/meta.tsx
+++ components/meta.tsx
@@ -20,16 +20,15 @@ const Meta = ({ title, description, image, keywords }) => {
         sizes="16x16"
         href="/favicon.png"
       />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
       <link
         rel="mask-icon"
         href="/favicon.png"
         color="#000000"
       />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="msapplication-TileColor" content="#db2777" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
+      <meta name="theme-color" content="#db2777" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <title>{title || 'Trckfi'}</title>
       <meta
@@ -38,6 +37,8 @@ const Meta = ({ title, description, image, keywords }) => {
       />
       <meta name="keywords" content={keywords || "financial success, spend tracking, budgeting, expense tracking, budget planning, financial health, tips, tools, financial freedom, personal finance, financial management, financial goals, financial success"}/>
       <meta property="og:image" content={image || "/trckfi-black-sm.png"} />
+      <meta property="og:title" content={title || 'Trckfi'} />
+      <meta property="og:description" content={description || "Take control of your wealth journey while keeping your data private & secure with Trckfi"} />
     </Head>
   )
 }
