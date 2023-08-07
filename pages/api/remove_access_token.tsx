47f4f21bddb23b4f2ffa3215c98ef944b5c17c6b
Tim Moreton <tmoreton89@gmail.com>
2023-08-07 11:18:14 -0400

    fix hide

--- pages/api/remove_access_token.tsx
+++ pages/api/remove_access_token.tsx
@@ -5,44 +5,45 @@ import prisma from '../../lib/prisma';
 export default async (req, res) => {
   const { account } = req.body
   try {
+    console.log(account)
     const response = await plaidClient.itemRemove({
       access_token: account.plaid.access_token
     })
     
-    if(response.data){
-      await prisma.plaid.updateMany({
-        where: {
-          item_id: account.item_id
-        },
-        data: {
-          active: false,
-          user_id: null
-        }
-      })
-      await prisma.accounts.updateMany({
-        where: {
-          item_id: account.item_id
-        },
-        data: {
-          active: false,
-          user_id: null,
-          account_id: null
-        }
-      })
-      await prisma.transactions.updateMany({
-        where: {
-          item_id: account.item_id
-        },
-        data: {
-          active: false,
-          user_id: null,
-          transaction_id: null
-        }
-      })
-    }
+    await prisma.plaid.updateMany({
+      where: {
+        item_id: account.item_id
+      },
+      data: {
+        active: false,
+        user_id: null
+      }
+    })
+    
+    await prisma.accounts.updateMany({
+      where: {
+        item_id: account.item_id
+      },
+      data: {
+        active: false,
+        user_id: null,
+        account_id: null
+      }
+    })
+
+    await prisma.transactions.updateMany({
+      where: {
+        item_id: account.item_id
+      },
+      data: {
+        active: false,
+        user_id: null,
+        transaction_id: null
+      }
+    })
     return res.status(200).json('ok')
   } catch (error) {
     console.error(error)
-    return res.status(500).json({ error: error.message || error.toString() })
+    // return res.status(500).json({ error: error.message || error.toString() })
   }
 }
