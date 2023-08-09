d384283d32d6ca7c4693c5ce1453a611a9d5fdf6
Tim Moreton <tmoreton89@gmail.com>
2023-08-09 10:49:40 -0400

    fix removing unlinked accounts

--- pages/api/remove_access_token.tsx
+++ pages/api/remove_access_token.tsx
@@ -6,41 +6,65 @@ export default async (req, res) => {
   const { account } = req.body
   try {
 
-    if(account.plaid.error_code !== 'ITEM_NOT_FOUND'){
-      await plaidClient.itemRemove({
-        access_token: account.plaid.access_token
+    if(!account?.plaid){
+      await prisma.accounts.updateMany({
+        where: {
+          id: account.id
+        },
+        data: {
+          active: false,
+          user_id: null,
+          account_id: null
+        }
+      })
+      await prisma.transactions.updateMany({
+        where: {
+          id: account.id
+        },
+        data: {
+          active: false,
+          user_id: null,
+          transaction_id: null
+        }
+      })
+    } else {
+      if(account?.plaid?.error_code !== 'ITEM_NOT_FOUND'){
+        await plaidClient.itemRemove({
+          access_token: account.plaid.access_token
+        })
+      }
+  
+      await prisma.plaid.updateMany({
+        where: {
+          item_id: account.item_id
+        },
+        data: {
+          active: false,
+          user_id: null
+        }
+      })
+      await prisma.accounts.updateMany({
+        where: {
+          item_id: account.item_id
+        },
+        data: {
+          active: false,
+          user_id: null,
+          account_id: null
+        }
+      })
+      await prisma.transactions.updateMany({
+        where: {
+          item_id: account.item_id
+        },
+        data: {
+          active: false,
+          user_id: null,
+          transaction_id: null
+        }
       })
     }
-
-    await prisma.plaid.updateMany({
-      where: {
-        item_id: account.item_id
-      },
-      data: {
-        active: false,
-        user_id: null
-      }
-    })
-    await prisma.accounts.updateMany({
-      where: {
-        item_id: account.item_id
-      },
-      data: {
-        active: false,
-        user_id: null,
-        account_id: null
-      }
-    })
-    await prisma.transactions.updateMany({
-      where: {
-        item_id: account.item_id
-      },
-      data: {
-        active: false,
-        user_id: null,
-        transaction_id: null
-      }
-    })
+    
     return res.status(200).json('ok')
   } catch (error) {
     console.error(error)
