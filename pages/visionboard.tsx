040978a0a5ad042bd65af3e04033d0b74691a407
Tim Moreton <tmoreton89@gmail.com>
2023-08-10 15:09:35 -0400

    add new vision

--- pages/visionboard.tsx
+++ pages/visionboard.tsx
@@ -5,7 +5,6 @@ import Menu from '../components/menu'
 import Meta from '../components/meta'
 import ConfettiExplosion from 'react-confetti-explosion'
 import { useSession } from "next-auth/react"
-import VisionModal from '../components/modals/vision-modal'
 import  { useLocalStorage } from '../utils/useLocalStorage'
 
 const Editor = dynamic(() => import('../components/editor'), { ssr: false })
@@ -14,13 +13,11 @@ export default function ({ showError }) {
   const { data: session } = useSession()
   const user = session?.user
   const [showConfetti, setConfetti] = useLocalStorage('showConfetti', '')
-  const [open, openModal] = useLocalStorage('visionModal', '')
 
   useEffect(() => {
     // @ts-ignore
     if(user?.login_count <= 1){
       if(!showConfetti) setConfetti(true)
-      // if(!open) openModal(true)
     } 
   }, [])
 
@@ -35,7 +32,6 @@ export default function ({ showError }) {
           keywords=''
         />
         <Editor />
-        <VisionModal open={open} openModal={openModal} />
         { showConfetti && <ConfettiExplosion force={0.5} duration={3000} particleCount={500} width={3500} zIndex={1000}/>}
       </DashboardLayout>
     </>
