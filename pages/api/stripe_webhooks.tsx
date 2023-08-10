ee1dbd0aba10eb7617ab175343fa73960e4de2e2
Tim Moreton <tmoreton89@gmail.com>
2023-08-10 10:22:18 -0400

    remove micro

--- pages/api/stripe_webhooks.tsx
+++ pages/api/stripe_webhooks.tsx
@@ -1,38 +1,22 @@
 import Stripe from 'stripe'
-import { buffer } from 'micro'
 import getRawBody from 'raw-body'
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2022-11-15',
 })
 
-// const payload = {
-//   id: 'evt_test_webhook',
-//   object: 'event',
-// };
-
-// const payloadString = JSON.stringify(payload, null, 2);
-// const secret = 'whsec_test_secret';
-
-// const header = stripe.webhooks.generateTestHeaderString({
-//   payload: payloadString,
-//   secret,
-// });
 export const config = {
   api: {
     bodyParser: false,
   },
 };
-// const event = stripe.webhooks.constructEvent(payloadString, header, secret);
+
 export default async (req, res) => {
   try {
     const sig = req.headers['stripe-signature'];
-    // const rawBody = await buffer(req)
-    // const body = JSON.parse(rawBody.toString())
-    // console.error("Yay, we got the body back", {body})
     const rawBody = await getRawBody(req);
-    const event = stripe.webhooks.constructEvent(rawBody, sig, 'whsec_vmD4RnQOmfPQGdeTTheDOGfGNgUEJ2k0');
-
-    console.error(event)
+    const event = stripe.webhooks.constructEvent(rawBody, sig, process.env.STRIPE_WEBHOOK_SECRET);
+    console.log(event)
     switch (event.type) {
       case 'customer.subscription.created':
         const customerSubscriptionCreated = event.data.object;
