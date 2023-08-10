b858db8d6c020af7489510d20f33697dc6ffc11b
Tim Moreton <tmoreton89@gmail.com>
2023-08-10 13:25:30 -0400

    auto sync transactions on new plaid

--- pages/visionboard.tsx
+++ pages/visionboard.tsx
@@ -5,6 +5,7 @@ import Menu from '../components/menu'
 import Meta from '../components/meta'
 import ConfettiExplosion from 'react-confetti-explosion'
 import { useSession } from "next-auth/react"
+import VisionModal from '../components/modals/vision-modal'
 
 const Editor = dynamic(() => import('../components/editor'), { ssr: false })
 
@@ -12,10 +13,14 @@ export default function ({ showError }) {
   const { data: session } = useSession()
   const user = session?.user
   const [showConfetti, setConfetti] = useState(false)
+  const [open, openModal] = useState(false)
 
   useEffect(() => {
     // @ts-ignore
-    if(user?.login_count <= 1) setConfetti(true)
+    if(user?.login_count <= 1){
+      setConfetti(true)
+      openModal(true)
+    } 
   }, [])
 
   return (
@@ -29,6 +34,7 @@ export default function ({ showError }) {
           keywords=''
         />
         <Editor />
+        <VisionModal open={open} openModal={openModal} />
         { showConfetti && <ConfettiExplosion force={0.5} duration={3000} particleCount={500} width={3500} zIndex={1000}/>}
       </DashboardLayout>
     </>
