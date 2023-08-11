9a7e31a258f836f5668a8d6c4f0bca1bc0845c17
Tim Moreton <tmoreton89@gmail.com>
2023-08-11 13:07:39 -0400

    fix accounts and add mobile styling

--- components/menu.tsx
+++ components/menu.tsx
@@ -13,7 +13,6 @@ import  { clearLocalStorage } from '../utils/useLocalStorage'
 
 const navigation = [
   { name: 'Home', href: '/' },
-  { name: 'FAQ', href: '/faq' },
   // { name: 'Pricing', href: '/pricing' },
   { name: 'Blog', href: '/blog' },
   { name: 'FAQ', href: '/faq' },
