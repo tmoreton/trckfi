753d21d598604761edc6d36b49f228b7b4042e7c
Tim Moreton <tmoreton89@gmail.com>
2023-08-13 13:23:37 -0400

    add has_more

--- pages/api/sync_accounts.jsx
+++ pages/api/sync_accounts.jsx
@@ -39,51 +39,55 @@ export default async (req, res) => {
   })
   
   for (let p in plaid) {
-    const request = {
-      access_token: plaid[p].access_token,
-      cursor: plaid[p].cursor || '',
-      count: 250,
-      options: {
-        include_personal_finance_category: true
-      }
-    }
     try {
-      const response = await plaidClient.transactionsSync(request)
-      let added = response.data.added
-      let next_cursor = response.data.next_cursor
-      // let has_more = response.data.has_more
-      for (let i in added) {
-        let { id, type } = plaid[p]?.accounts.find(a => a.account_id === added[i].account_id)
-        let detailed_category = added[i].personal_finance_category.detailed.replace(`${added[i].personal_finance_category.primary}_`, '')
-        let { amount } = formatAmount(type, added[i].amount)
-        let rule = rules.find(r => added[i].name.toUpperCase().includes(r.identifier.toUpperCase()))
-        await prisma.transactions.upsert({
-          where: { 
-            transaction_id: added[i].transaction_id 
-          },
-          update: {},
-          create: {
-            amount,
-            account_id: id,
-            transaction_id: added[i].transaction_id,
-            authorized_date: new Date(added[i].date),
-            date: added[i].date,
-            name: rule?.ruleset?.name || added[i].name,
-            merchant_name: added[i].merchant_name,
-            category: added[i].category,
-            detailed_category: rule?.ruleset?.detailed_category && snakeCase(rule?.ruleset?.detailed_category).toUpperCase() || detailed_category,
-            unified: icons[detailed_category],
-            primary_category: rule?.ruleset?.primary_category && snakeCase(rule?.ruleset?.primary_category).toUpperCase() || added[i].personal_finance_category.primary,
-            location: added[i].location,
-            user_id: user.id,
-            currency: added[i].iso_currency_code,
-            item_id: plaid[p].item_id,
-            month_year: added[i].date.substring(0,7),
-            week_year: `${added[i].date.substring(0,4)}-${DateTime.fromISO(added[i].date).weekNumber}`,
-            active: rule?.ruleset?.active && (rule?.ruleset?.active === 'true') || true,
-            recurring: rule?.ruleset?.recurring && (rule?.ruleset?.recurring === 'true') || false,
-          },
-        })
+      let has_more = true
+      let next_cursor = plaid[p].cursor || ''
+      while (has_more) {
+        const request = {
+          access_token: plaid[p].access_token,
+          cursor: next_cursor,
+          count: 250,
+          options: {
+            include_personal_finance_category: true
+          }
+        }
+        const response = await plaidClient.transactionsSync(request)
+        let added = response.data.added
+        next_cursor = response.data.next_cursor
+        has_more = response.data.has_more
+        for (let i in added) {
+          let { id, type } = plaid[p]?.accounts.find(a => a.account_id === added[i].account_id)
+          let detailed_category = added[i].personal_finance_category.detailed.replace(`${added[i].personal_finance_category.primary}_`, '')
+          let { amount } = formatAmount(type, added[i].amount)
+          let rule = rules.find(r => added[i].name.toUpperCase().includes(r.identifier.toUpperCase()))
+          await prisma.transactions.upsert({
+            where: { 
+              transaction_id: added[i].transaction_id 
+            },
+            update: {},
+            create: {
+              amount,
+              account_id: id,
+              transaction_id: added[i].transaction_id,
+              authorized_date: new Date(added[i].date),
+              date: added[i].date,
+              name: rule?.ruleset?.name || added[i].name,
+              merchant_name: added[i].merchant_name,
+              category: added[i].category,
+              detailed_category: rule?.ruleset?.detailed_category && snakeCase(rule?.ruleset?.detailed_category).toUpperCase() || detailed_category,
+              unified: icons[detailed_category],
+              primary_category: rule?.ruleset?.primary_category && snakeCase(rule?.ruleset?.primary_category).toUpperCase() || added[i].personal_finance_category.primary,
+              location: added[i].location,
+              user_id: user.id,
+              currency: added[i].iso_currency_code,
+              item_id: plaid[p].item_id,
+              month_year: added[i].date.substring(0,7),
+              week_year: `${added[i].date.substring(0,4)}-${DateTime.fromISO(added[i].date).weekNumber}`,
+              active: rule?.ruleset?.active && (rule?.ruleset?.active === 'true') || true,
+              recurring: rule?.ruleset?.recurring && (rule?.ruleset?.recurring === 'true') || false,
+            },
+          })
+        }
       }
 
       const accountResponse = await plaidClient.accountsGet({ access_token: plaid[p].access_token })
