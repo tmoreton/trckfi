9a49b28ce2297cd10af5e527f926404dd1834b32
Tim Moreton <tmoreton89@gmail.com>
2023-08-13 22:10:58 -0400

    fix custom rules

--- pages/rules.tsx
+++ pages/rules.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import DashboardLayout from "../components/dashboard-layout"
 import { useSession } from "next-auth/react"
 import { XCircleIcon } from '@heroicons/react/24/outline'
@@ -8,6 +8,14 @@ import  { useLocalStorage } from '../utils/useLocalStorage'
 import Menu from '../components/menu'
 import Meta from '../components/meta'
 
+const keys = {
+  name: 'Name',
+  primary_category: 'Primary Category',
+  detailed_category: 'Detailed Category',
+  recurring: 'Recurring',
+  active: 'Active',
+}
+
 const Rules = ({ showError }) => {
   const { data: session } = useSession()
   const user = session?.user
@@ -15,6 +23,8 @@ const Rules = ({ showError }) => {
   const [ruleset, setRuleset] = useState(null)
   const [rules, setRules] = useLocalStorage('rules', [])
   const [alerts, setAlerts] = useLocalStorage('alerts', [])
+  const [, updateState] = useState();
+  const forceUpdate = useCallback(() => updateState({}), []);
 
   useEffect(() => {
     getRules()
@@ -48,12 +58,14 @@ const Rules = ({ showError }) => {
   }
 
   const addRule = async () => {
+    let newRuleset = ruleset
+    delete newRuleset['']
     const res = await fetch(`/api/add_rule`, {
       body: JSON.stringify({
         // @ts-ignore
         user_id: user?.id,
         identifier,
-        ruleset
+        ruleset: newRuleset
       }),
       headers: {
         'Content-Type': 'application/json',
@@ -81,6 +93,15 @@ const Rules = ({ showError }) => {
     const { error } = await res.json()
     showError(error)
     if(!error) getRules()
+  }
+  
+  const removeItem = async (key) => {
+    if(key){
+      let item = ruleset
+      delete item[key]
+      setRuleset(item)
+      forceUpdate()
+    }
   }
 
   const removeAlert = async (id) => {
@@ -143,8 +164,8 @@ const Rules = ({ showError }) => {
                   </div>
                 ))}
                 {ruleset ?
-                  <div className="pt-6 sm:flex items-center">
-                    <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
+                  <div className="pt-6 sm:flex items-start">
+                    <dt className="font-medium text-gray-900 w-[350px] sm:flex-none sm:pr-6">
                       <label htmlFor="email" className="block text-sm font-small leading-6 text-pink-600">
                         Identifier
                       </label>
@@ -156,33 +177,36 @@ const Rules = ({ showError }) => {
                         value={identifier}
                         onChange={e => setIdentifier(e.target.value)}
                         required
+                        placeholder="Add Transaction name"
                         className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-pink-600 peer" 
                       />
                     </dt>
                     <dd className="mt-1 block justify-start gap-x-6 sm:mt-0 sm:flex-auto items-end">
                       <div className="mt-1 flex justify-start gap-x-6 sm:mt-0 sm:flex-auto items-end">
-                        <label htmlFor="rule" className="block text-sm font-medium leading-6 text-pink-600 w-[185px]">
+                        <label htmlFor="rule" className="block text-sm font-medium leading-6 text-pink-600 sm:w-64">
                           Rules
                         </label>
                         <label htmlFor="email" className="block text-sm font-medium leading-6 text-pink-600">
                           Value
                         </label>
                       </div>
+                      <select
+                        name="rule"
+                        value=''
+                        onChange={e => setRuleset({ ...ruleset, [e.target.value]: '' })}
+                        className="mt-2 block sm:w-64 rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-pink-600 sm:text-sm sm:leading-6"
+                      >
+                        <option className="text-gray-500" value="" label="Select Value..." />
+                        <option value="name" label="Name" />
+                        <option value="primary_category" label="Primary Category" />
+                        <option value="detailed_category" label="Detailed Category" />
+                        <option value="recurring" label="Recurring" />
+                        <option value="active" label="Active" />
+                      </select>
                       {Object.keys(ruleset).map((i, key) => (
-                        <div key={key} className="mt-1 flex justify-start gap-x-6 sm:mt-0 sm:flex-auto items-end">
+                        <div key={i} className="mt-1 flex justify-start gap-x-6 sm:mt-0 sm:flex-auto items-center">
                           <div>
-                            <select
-                              name="rule"
-                              value={i}
-                              onChange={e => setRuleset({ ...ruleset, [e.target.value]: '' })}
-                              className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-pink-600 sm:text-sm sm:leading-6"
-                            >
-                              <option value="name" label="Name" />
-                              <option value="primary_category" label="Primary Category" />
-                              <option value="detailed_category" label="Detailed Category" />
-                              <option value="recurring" label="Recurring" />
-                              <option value="active" label="Active" />
-                            </select>
+                            {i && <p className="mt-2 block sm:w-64 py-1.5 pl-3 pr-10 text-gray-900 sm:text-sm sm:leading-6">{keys[i]}</p>}
                           </div>
                           <div className="font-medium text-gray-900 sm:w-64 sm:flex-none">
                             { i === 'recurring' || i === 'active' ?
@@ -197,17 +221,21 @@ const Rules = ({ showError }) => {
                                 <option value="false" label="False" />
                               </select>
                               :
-                              <input
-                                name={i}
-                                onChange={e => setRuleset({ ...ruleset, [i]: e.target.value })}
-                                required
-                                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-pink-600 peer" 
-                              />
+                              <>
+                              {i && 
+                                <input
+                                  name={i}
+                                  onChange={e => setRuleset({ ...ruleset, [i]: e.target.value })}
+                                  required
+                                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-pink-600 peer" 
+                                />
+                              }
+                              </>
                             }
                           </div>
-                          <button onClick={() => setRuleset(null)} type="button">
+                          {i && <button onClick={() => removeItem(i)} type="button" >
                             <XCircleIcon className='text-red-400 group-hover:text-red-600 h-5 w-5 shrink-0' />
-                          </button>
+                          </button>}
                         </div>
                       ))}
                     </dd>
@@ -217,7 +245,7 @@ const Rules = ({ showError }) => {
                   </div>
                   :
                   <div className="flex pt-6">
-                    <button onClick={() => setRuleset({'name': ''})} type="button" className="text-sm font-semibold leading-6 text-pink-600 hover:text-pink-500">
+                    <button onClick={() => setRuleset({'': ''})} type="button" className="text-sm font-semibold leading-6 text-pink-600 hover:text-pink-500">
                       <span aria-hidden="true">+</span> Create Rule
                     </button>
                   </div>
@@ -236,7 +264,7 @@ const Rules = ({ showError }) => {
                   <div className="pt-6 sm:flex">
                     <dt className="font-bold text-gray-900 sm:w-64 sm:flex-none sm:pr-6">{alert.name} - <span className="font-normal">{DateTime.fromISO(alert.alert_date).toLocaleString(DateTime.DATE_FULL)}</span></dt>
                     <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-                      <div className="text-gray-900"><span className="font-semibold">Notes:</span> {alert.notes}</div>
+                      { alert.notes && <div className="text-gray-900"><span className="font-semibold">Notes:</span> {alert.notes}</div>}
                       <button onClick={() => removeAlert(alert.id)} type="button" className="font-semibold text-pink-600 hover:text-pink-500">
                         Remove
                       </button>
