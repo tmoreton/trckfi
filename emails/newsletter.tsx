fa23b512ba147384d97dd85274016c43ea830138
Tim Moreton <tmoreton89@gmail.com>
2023-08-17 11:34:09 -0400

    update subs

--- emails/newsletter.tsx
+++ emails/newsletter.tsx
@@ -1,50 +1,47 @@
 import {
-    Body,
-    Button,
-    Container,
-    Head,
-    Heading,
-    Hr,
-    Html,
-    Img,
-    Preview,
-    Section,
-    Tailwind,
-    Text,
-  } from '@react-email/components';
-  import * as React from 'react';
-  
-  export const Newsletter = () => {
-    return (
-      <Html>
-        <Head />
-        <Preview>Trckfi - Newsletter</Preview>
-        <Tailwind>
-          <Body className="bg-white my-auto mx-auto font-sans">
-            <Container className="my-[40px] mx-auto p-[20px] w-[465px]">
-              <Section>
-                <Img
-                  src='https://www.trckfi.com/trckfi.png'
-                  width="75"
-                  height="75"
-                  alt="Trckfi"
-                  className="my-0 mx-auto"
-                />
-              </Section>
-              <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
-                Thanks for subscribing to <strong>Trckfi!</strong>
-              </Heading>
-              <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
-              <Text className="text-[#666666] text-[12px] leading-[24px]">
-                If you were not expecting this invitation, you can ignore this email.
-                If you are concerned about your account's safety, please reply 
-                to this email to get in touch with us.
-              </Text>
-            </Container>
-          </Body>
-        </Tailwind>
-      </Html>
-    );
-  };
-  
-  export default Newsletter;
+  Body,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Tailwind,
+  Text,
+} from '@react-email/components';
+import * as React from 'react';
+
+export default function (){
+  return (
+    <Html>
+      <Head />
+      <Preview>Trckfi - Newsletter</Preview>
+      <Tailwind>
+        <Body className="bg-white my-auto mx-auto font-sans">
+          <Container className="my-[40px] mx-auto p-[20px] w-[465px]">
+            <Section>
+              <Img
+                src='https://www.trckfi.com/trckfi.png'
+                width="75"
+                height="75"
+                alt="Trckfi"
+                className="my-0 mx-auto"
+              />
+            </Section>
+            <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
+              Thanks for subscribing to <strong>Trckfi!</strong>
+            </Heading>
+            <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
+            <Text className="text-[#666666] text-[12px] leading-[24px]">
+              If you were not expecting this invitation, you can ignore this email.
+              If you are concerned about your account's safety, please reply 
+              to this email to get in touch with us.
+            </Text>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
