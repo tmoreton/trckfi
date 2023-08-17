655fc443cbe97c92819816fe8f424fddad291d3f
Tim Moreton <tmoreton89@gmail.com>
2023-08-16 22:42:18 -0400

    send email on newsletter

--- pages/api/create_user.tsx
+++ pages/api/create_user.tsx
@@ -1,5 +1,8 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 import prisma from '../../lib/prisma';
+import nodemailer from 'nodemailer'
+import { render } from '@react-email/render'
+import Newsletter from "../../emails/newsletter"
 
 export default async (req, res) => {
   const { email } = req.body
@@ -8,12 +11,35 @@ export default async (req, res) => {
   }
 
   try {
-    const user = await prisma.user.upsert({
+    await prisma.user.upsert({
       where: { email: email.toLowerCase() },
       update: { email: email.toLowerCase() },
       create: { email: email.toLowerCase() },
     })
     
+    const emailHtml = render(
+      <Newsletter />
+    )
+
+    const message = {
+      from: `"Trckfi" <${process.env.EMAIL_ADDRESS}>`,
+      to: email.toLowerCase(),
+      subject: `Thanks for subscribing!`,
+      text: '',
+      html: emailHtml,
+    }
+
+    let transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    })
+
+    await transporter.sendMail(message)
+
     return res.status(200).json({ status: 'OK' })
   } catch (error) {
     console.error(error)
