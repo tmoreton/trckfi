a202acd68e782eb33550033919071e4ea8631134
Tim Moreton <tmoreton89@gmail.com>
2023-08-18 12:43:23 -0400

    copy change

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -151,7 +151,7 @@ const Settings = ({ showError }) => {
                 onClick={() => {navigator.clipboard.writeText(`${process.env['NEXT_PUBLIC_BASE_URL']}/signup?referral_id=${user?.referral_id}`)}}
                 className="rounded-md bg-pink-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600"
               >
-                Copy Link
+                Copy Referral Link
               </button>
             </div>
           </div>
