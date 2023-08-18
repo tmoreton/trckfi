d20ca5b449fda1b12b01e5c7982ae2a5c7232e56
Tim Moreton <tmoreton89@gmail.com>
2023-08-18 12:21:16 -0400

    update space

--- pages/api/stripe_webhooks.tsx
+++ pages/api/stripe_webhooks.tsx
@@ -51,7 +51,7 @@ export default async (req, res) => {
     return res.status(200).json({ data: event })
   } catch (error) {
     console.error(error)
-throw new Error(error)
+    throw new Error(error)
     return res.status(500).json({ error: error.message || error.toString() })
   }
 }
