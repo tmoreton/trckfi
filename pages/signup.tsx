f26dfe6fdac75f1b4e4cb53ff36f20fd806f596f
Tim Moreton <tmoreton89@gmail.com>
2023-08-18 13:54:16 -0400

    testing api

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import type { GetServerSidePropsContext } from "next";
 import { getCsrfToken } from "next-auth/react"
 import Icon from '../components/icon';
@@ -10,9 +10,10 @@ import prisma from '../lib/prisma'
 import { new_vision } from '../utils/default-vision'
 
 export default function ({ csrfToken, user, showError, referral_id }) {
-  const email = user?.email
+  const [checked, setChecked] = useState(false)
+  const [email, setEmail] = useState('')
 
-  const handleSubmit = async (email) => {
+  const checkout = async (email) => {
     const res = await fetch(`/api/checkout_session`, {
       body: JSON.stringify({ 
         email,
@@ -24,6 +25,7 @@ export default function ({ csrfToken, user, showError, referral_id }) {
       method: 'POST',
     })
     const data = await res.json()
+    console.log(data)
     showError(data.error)
     if (data.error) return
 
@@ -34,13 +36,30 @@ export default function ({ csrfToken, user, showError, referral_id }) {
     showError(error)
   }
 
-  useEffect(() => {
-    if(email){
-      handleSubmit(email)
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if(checked){
+      const res = await fetch(`/api/auth/signin/email?callbackUrl=${process.env['NEXT_PUBLIC_BASE_URL']}/signup?referral_id=${referral_id}`, {
+        body: JSON.stringify({ 
+          email,
+          csrfToken
+        }),
+        method: 'POST',
+      })
+      const { data, error } = await res.json()
+      showError(error)
+    } else {
+      showError('Please agree to terms.')
     }
-  }, [email])
+  }
 
-  if (!email) return (
+  useEffect(() => {
+    if(user?.email){
+      checkout(user?.email)
+    }
+  }, [user?.email])
+
+  if (!user?.email) return (
     <>
       <Meta
         title="Getting Started"
@@ -61,7 +80,7 @@ export default function ({ csrfToken, user, showError, referral_id }) {
           </div>
         </div>
         <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-3" method="post" action={`/api/auth/signin/email?callbackUrl=${process.env['NEXT_PUBLIC_BASE_URL']}/signup?referral_id=${referral_id}`}>
+          <form className="space-y-3" onSubmit={handleSubmit}>
             <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
             <label htmlFor="email" className="block text-sm font-small leading-6 text-pink-600">
               Email address
@@ -71,11 +90,35 @@ export default function ({ csrfToken, user, showError, referral_id }) {
                 id="email"
                 name="email"
                 type="email"
+                onChange={e => setEmail(e.target.value)}
                 autoComplete="email"
                 required
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-pink-600 peer" 
               />
             </div>
+            <fieldset>
+              <legend className="sr-only">Terms of Service</legend>
+              <div className="relative flex items-center justify-center py-2">
+                <div className="flex h-6 items-center">
+                  <input
+                    id="terms"
+                    aria-describedby="terms"
+                    name="terms"
+                    type="checkbox"
+                    onChange={(e) => setChecked(e.target.checked)}
+                    className="h-3 w-3 rounded border-gray-300 text-pink-600 focus:ring-pink-600"
+                  />
+                </div>
+                <div className="ml-3 text-sm leading-6">
+                  <span id="terms" className="text-gray-500">
+                    I agree to Trckfi's
+                  </span>
+                  <label htmlFor="terms" className="ml-1 font-medium text-gray-900">
+                    <a href="/terms-of-use" target="_blank">Terms of Service</a>
+                  </label>
+                </div>
+              </div>
+            </fieldset>
             <button
               type="submit"
               className="flex text-center justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600 mx-auto"
