3a54f6236f57f153996f0b9aa2ee332891a38ce0
Tim Moreton <tmoreton89@gmail.com>
2023-08-19 11:52:59 -0400

    add terms to checkout

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -8,6 +8,7 @@ import Menu from '../components/menu'
 import Meta from '../components/meta'
 import { useRouter } from 'next/router'
 import  { clearLocalStorage } from '../utils/useLocalStorage'
+import ConfettiExplosion from 'react-confetti-explosion'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -21,6 +22,7 @@ const Settings = ({ showError }) => {
   const [sendBtn, setSendBtn] = useState('Send Invite')
   const [email, setEmail] = useState('')
   const [linkedUser, setLinkedUser] = useState({})
+  const [link, setLink] = useState(null)
   const router = useRouter()
 
   useEffect(() => {
@@ -124,6 +126,13 @@ const Settings = ({ showError }) => {
     }
   }
 
+  const copyLink = () => {
+    // @ts-ignore
+    let url = `${process.env['NEXT_PUBLIC_BASE_URL']}/signup?referral_id=${user?.referral_id}`
+    setLink(url)
+    navigator.clipboard.writeText(url)
+  }
+
   // @ts-ignore
   let { email_weekly, email_monthly, email_alert } = preferences 
   return (
@@ -147,12 +156,12 @@ const Settings = ({ showError }) => {
             </div>
             <div className="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
               <button
-                // @ts-ignore
-                onClick={() => {navigator.clipboard.writeText(`${process.env['NEXT_PUBLIC_BASE_URL']}/signup?referral_id=${user?.referral_id}`)}}
+                onClick={copyLink}
                 className="rounded-md bg-pink-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600"
               >
-                Copy Referral Link
+                { link ? 'Copied!' : 'Copy Referral Link'}
               </button>
+              { link && <ConfettiExplosion />}
             </div>
           </div>
         </div>
