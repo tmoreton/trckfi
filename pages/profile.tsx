627f2cb00e1588cdeed4ca0d397b57fc35464c21
Tim Moreton <tmoreton89@gmail.com>
2023-08-23 09:21:02 -0400

    added copy response

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -26,6 +26,7 @@ const Profile = ({ showError }) => {
   const [sendBtn, setSendBtn] = useState('Send Invite')
   const [email, setEmail] = useState('')
   const [linkedUser, setLinkedUser] = useState({})
+  const [link, setLink] = useState('')
   const router = useRouter()
 
   useEffect(() => {
@@ -150,6 +151,13 @@ const Profile = ({ showError }) => {
     }
   }
 
+  const copyLink = () => {
+    // @ts-ignore
+    let url = `${process.env['NEXT_PUBLIC_BASE_URL']}/signup?referral_id=${user?.referral_id}`
+    setLink(url)
+    navigator.clipboard.writeText(url)
+  }
+
   // @ts-ignore
   let { email_weekly, email_monthly, email_alert } = preferences
   return (
@@ -172,11 +180,11 @@ const Profile = ({ showError }) => {
             </div>
             <div className="mt-10 items-center text-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
               <button
-                // @ts-ignore
-                onClick={() => {navigator.clipboard.writeText(`${process.env['NEXT_PUBLIC_BASE_URL']}/signup?referral_id=${user?.referral_id}`)}}
+                
+                onClick={copyLink}
                 className="rounded-md bg-pink-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600"
               >
-                Copy Referral Link
+                {link ? 'Copied! 🎉' : 'Copy Referral Link'}
               </button>
               { // @ts-ignore
                 !user.customer_id && <p className="text-xs text-gray-500 pt-3">Any credits will get added to the account owner</p>
