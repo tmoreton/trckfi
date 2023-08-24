eb6f4baa1239bfd9a52fd23299f12d6c3dad17fa
Tim Moreton <tmoreton89@gmail.com>
2023-08-24 11:09:41 -0400

    add plaid webhook

--- pages/accounts.tsx
+++ pages/accounts.tsx
@@ -66,27 +66,29 @@ const Accounts = ({ showError }) => {
   }
 
   const syncPlaid = async (access_token) => {
-    setRefreshing(true)
-    const res = await fetch(`/api/sync_plaid`, {
-      body: JSON.stringify({
-        user,
-        access_token
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-    })
-    const { error } = await res.json()
-    showError(error)
-    if(!error) {
-      getAccounts()
-      setRefreshing(false)
-      setConfetti(true)
-    }
+    getAccounts()
+    
+    // const res = await fetch(`/api/sync_plaid`, {
+    //   body: JSON.stringify({
+    //     user,
+    //     access_token
+    //   }),
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   method: 'POST',
+    // })
+    // const { error } = await res.json()
+    // showError(error)
+    // if(!error) {
+    //   getAccounts()
+    //   setRefreshing(false)
+    //   setConfetti(true)
+    // }
   }
 
   const getAccounts = async () => {
+    setRefreshing(true)
     const res = await fetch(`/api/get_accounts`, {
       body: JSON.stringify({
         user
@@ -98,13 +100,14 @@ const Accounts = ({ showError }) => {
     })
     const { error, data } = await res.json()
     showError(error)
-    setRefreshing(false)
     const accounts = data.reduce(function (r, a) {
       r[a.institution] = r[a.institution] || [];
       r[a.institution].push(a);
       return r;
     }, Object.create(null))
     setAccounts(accounts)
+    setRefreshing(false)
+    setConfetti(true)
   }
 
   const refresh = async () => {
