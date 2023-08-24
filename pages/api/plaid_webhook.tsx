4d4dabad4f3004bda21a0a76f376f8febd3a2c1e
Tim Moreton <tmoreton89@gmail.com>
2023-08-24 09:44:22 -0400

    add hardware

--- pages/api/plaid_webhook.tsx
+++ pages/api/plaid_webhook.tsx
@@ -9,9 +9,9 @@ export const config = {
 export default async (req, res) => {
   // const { webhook_code, item_id, } = req.body
   let test = 'SYNC_UPDATES_AVAILABLE'
-  console.log('req', req)
+  // console.log('req', req)
   const rawBody = await getRawBody(req)
-  console.log('rawBody', rawBody)
+  console.log(rawBody)
   switch (test) {
     case 'SYNC_UPDATES_AVAILABLE': {
       break;
