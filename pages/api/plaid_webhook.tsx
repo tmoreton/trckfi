08d8e23bbe9550980e8cd2bcd546a4304d484b10
Tim Moreton <tmoreton89@gmail.com>
2023-08-24 11:34:24 -0400

    add plaid webhook

--- pages/api/plaid_webhook.tsx
+++ pages/api/plaid_webhook.tsx
@@ -5,17 +5,23 @@ import accountsSync from '../../utils/accountsSync';
 export default async (req, res) => {
   const { webhook_code, item_id } = req.body
   console.log(webhook_code, item_id)
+  const sync = async (item_id) => {
+    let { access_token, user_id } = await prisma.plaid.findUnique({ where: { item_id }})
+    let accounts = accountsSync({ access_token, item_id, user_id })
+    transactionsSync({ access_token, next_cursor: '', accounts, user_id})
+  }
   switch (webhook_code) {
     case 'SYNC_UPDATES_AVAILABLE':
-      let { access_token, user_id } = await prisma.plaid.findUnique({ where: { item_id }})
-      let accounts = accountsSync({ access_token, item_id, user_id })
-      transactionsSync({ access_token, next_cursor: '', accounts, user_id})
+      sync(item_id)
       break;
     case 'DEFAULT_UPDATE':
+      sync(item_id)
       break;
-    case 'INITIAL_UPDATE':
+    case 'INITIAL_UPDATE': 
+      sync(item_id)
       break;
-    case 'HISTORICAL_UPDATE':
+    case 'HISTORICAL_UPDATE': 
+    sync(item_id)
       break;
     default:
       break;
