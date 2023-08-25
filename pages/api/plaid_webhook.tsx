b1d2c5012081cc62a27604cbeb1703cf61ef0b72
Tim Moreton <tmoreton89@gmail.com>
2023-08-25 09:11:09 -0400

    abstract func

--- pages/api/plaid_webhook.tsx
+++ pages/api/plaid_webhook.tsx
@@ -2,14 +2,15 @@ import prisma from '../../lib/prisma';
 import transactionsSync from '../../utils/transactionsSync';
 import accountsSync from '../../utils/accountsSync';
 
+const sync = async (item_id) => {
+  let { access_token, user_id, institution } = await prisma.plaid.findUnique({ where: { item_id: item_id }})
+  let accounts = accountsSync({ access_token, item_id, user_id, institution })
+  transactionsSync({ access_token, next_cursor: '', accounts, user_id})
+}
+
 export default async (req, res) => {
   const { webhook_code, item_id } = req.body
   console.log(webhook_code, item_id)
-  const sync = async (item_id) => {
-    let { access_token, user_id, institution } = await prisma.plaid.findUnique({ where: { item_id: item_id }})
-    let accounts = accountsSync({ access_token, item_id, user_id, institution })
-    transactionsSync({ access_token, next_cursor: '', accounts, user_id})
-  }
   switch (webhook_code) {
     case 'SYNC_UPDATES_AVAILABLE':
       sync(item_id)
