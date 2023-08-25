ab6f3f4f9bbbcff787617fabd1cacccd02bdaf23
Tim Moreton <tmoreton89@gmail.com>
2023-08-25 12:30:00 -0400

    test new sync

--- pages/api/sync_test.tsx
+++ pages/api/sync_test.tsx
@@ -6,7 +6,8 @@ import accountsSync from '../../utils/accountsSync';
 export default async (req, res) => {
   let { access_token } = req.body
   try {
-    let { user_id, institution, item_id } = await prisma.plaid.findUnique({ where: { access_token }})
+    console.log(access_token)
+    let { user_id, institution, item_id } = await prisma.plaid.findUnique({ where: { access_token: access_token }})
     let accounts = accountsSync({ access_token, item_id, user_id, institution })
     transactionsSync({ access_token, next_cursor: '', accounts, user_id})
   } catch (error) {
