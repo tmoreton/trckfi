6b3b6a05da7ea96df86bf595da134ccd11b90a7a
Tim Moreton <tmoreton89@gmail.com>
2023-08-25 08:34:00 -0400

    declare item_id

--- pages/api/plaid_webhook.tsx
+++ pages/api/plaid_webhook.tsx
@@ -6,7 +6,7 @@ export default async (req, res) => {
   const { webhook_code, item_id } = req.body
   console.log(webhook_code, item_id)
   const sync = async (item_id) => {
-    let { access_token, user_id, institution } = await prisma.plaid.findUnique({ where: { item_id }})
+    let { access_token, user_id, institution } = await prisma.plaid.findUnique({ where: { item_id: item_id }})
     let accounts = accountsSync({ access_token, item_id, user_id, institution })
     transactionsSync({ access_token, next_cursor: '', accounts, user_id})
   }
