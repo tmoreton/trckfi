7a2b84fd66c2dd8d923e9da6a45d92f22739ec7e
Tim Moreton <tmoreton89@gmail.com>
2023-08-29 14:30:53 -0400

    update plaid

--- pages/api/sync_plaid.tsx
+++ pages/api/sync_plaid.tsx
@@ -1,14 +1,12 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 import prisma from '../../lib/prisma'
 import accountsSync from '../../utils/accountsSync'
-import transactionsSync from '../../utils/transactionsSync'
 import { client } from '../../trigger'
 
 export default async (req, res) => {
   let { access_token } = req.body
   let { institution, item_id, user_id } = await prisma.plaid.findUnique({ where: { access_token: access_token }})
   await accountsSync(access_token, item_id, user_id, institution)
-  // await transactionsSync(access_token, user_id)
   client.sendEvent({
     name: "sync.transactions",
     payload: { access_token, user_id },
