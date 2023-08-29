1bbb6877b12c92b30a0482c33f54a2d1b9923610
Tim Moreton <tmoreton89@gmail.com>
2023-08-29 09:20:39 -0400

    update task

--- utils/transactionsSync.tsx
+++ utils/transactionsSync.tsx
@@ -46,6 +46,7 @@ const transactionsSync = async (access_token, user_id) => {
     let removed = response.data.removed
     let has_more = response.data.has_more
     next_cursor = response.data.next_cursor
+    console.log(added)
     // Added Transactions
     while(has_more){
       for (let i in added) {
