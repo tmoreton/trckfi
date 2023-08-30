5d4430682ae6ea48abdfa4135e1e1e04cc27caea
Tim Moreton <tmoreton89@gmail.com>
2023-08-30 08:45:16 -0400

    remove unused api

--- pages/api/sync_plaid.tsx
+++ pages/api/sync_plaid.tsx
@@ -2,16 +2,11 @@
 import prisma from '../../lib/prisma'
 import accountsSync from '../../utils/accountsSync'
 import transactionsSync from '../../utils/transactionsSync'
-import { client } from '../../trigger'
 
 export default async (req, res) => {
   let { access_token } = req.body
   let { institution, item_id, user_id } = await prisma.plaid.findUnique({ where: { access_token: access_token }})
   await accountsSync(access_token, item_id, user_id, institution)
   transactionsSync(access_token, user_id)
-  // client.sendEvent({
-  //   name: "sync.transactions",
-  //   payload: { access_token, user_id },
-  // })
   return res.status(200).json({ status: "Ok" })
 }
