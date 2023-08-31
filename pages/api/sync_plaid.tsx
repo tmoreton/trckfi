ef95ee681be34a31e89ff5f999ced775740213f0
Tim Moreton <tmoreton89@gmail.com>
2023-08-31 15:15:15 -0400

    add recurirng to sync

--- pages/api/sync_plaid.tsx
+++ pages/api/sync_plaid.tsx
@@ -2,11 +2,13 @@
 import prisma from '../../lib/prisma'
 import accountsSync from '../../utils/accountsSync'
 import transactionsSync from '../../utils/transactionsSync'
+import recurringSync from '../../utils/recurringSync';
 
 export default async (req, res) => {
   let { access_token } = req.body
   let { institution, item_id, user_id } = await prisma.plaid.findUnique({ where: { access_token: access_token }})
   await accountsSync(access_token, item_id, user_id, institution)
   transactionsSync(access_token, user_id)
+  recurringSync(access_token)
   return res.status(200).json({ status: "Ok" })
 }
