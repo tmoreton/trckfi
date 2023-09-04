adc00f7506cd2577cda9d77422d8e6af5ac066a1
Tim Moreton <tmoreton89@gmail.com>
2023-09-04 13:10:59 -0400

    setimeout for sync plaid

--- pages/api/hide_account.tsx
+++ pages/api/hide_account.tsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 import prisma from '../../lib/prisma';
+import netWorthSync from '../../utils/netWorthSync'
 
 export default async (req, res) => {
   const { user_id, id } = req.body
@@ -35,7 +36,7 @@ export default async (req, res) => {
     //     is_active: false
     //   },
     // })
-
+    await netWorthSync(user_id)
     return res.status(200).json({ status: 'OK' })
   } catch (error) {
     console.error(error)
