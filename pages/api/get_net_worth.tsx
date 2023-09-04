d341642d61d9f5872e6de7df069c3b759f4626e4
Tim Moreton <tmoreton89@gmail.com>
2023-09-04 14:33:58 -0400

    update demo

--- pages/api/get_net_worth.tsx
+++ pages/api/get_net_worth.tsx
@@ -2,6 +2,7 @@
 import prisma from '../../lib/prisma';
 import { DateTime } from "luxon"
 import netWorthSync from '../../utils/netWorthSync';
+import { el } from 'date-fns/locale';
 
 export default async (req, res) => {
   const { user } = req.body
@@ -12,18 +13,39 @@ export default async (req, res) => {
   
   try {
     const data = await prisma.netWorth.findMany({
-      // where:{
-      //   OR: user_query,
-      // },
       where:{
-        user_id,
+        OR: user_query,
       },
       orderBy: {
         created_at: 'desc'
+      },
+      select: {
+        stats: true,
+        snapshot: true,
+        // @ts-ignore
+        date: true,
+        user_id: true,
+        created_at: true
       }
     })
-    let stats = data.length > 0 ? data[0] : {}
-    let history = data.reverse()
+    let array = []
+    if(linked_user_id){
+      data.forEach(i => {
+         // @ts-ignore
+        let index = array.findIndex(e => e.date == i.date)
+        if(index < 0){
+          array.push(i)
+        }  else {
+          array[index].stats = Object.assign(array[index].stats, i.stats)
+          array[index].snapshot = Object.assign(array[index].snapshot, i.snapshot)
+        }
+      })
+    } else {
+      array = data
+    }
+
+    let stats = array.length > 0 ? array[0] : {}
+    let history = array.reverse()
 
     return res.status(200).json({ history, stats })
   } catch (error) {
