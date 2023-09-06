3fe7e0610a4dba175960855fbc21a57de597fbbc
Tim Moreton <tmoreton89@gmail.com>
2023-09-06 10:26:11 -0400

    fix hp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Container from '../components/container'
 import Hero from '../components/hero'
 import Layout from '../components/layout'
 import Newsletter from '../components/newsletter'
@@ -10,7 +9,7 @@ import FeatureNetWorth from '../components/feature-net-worth'
 import Menu from '../components/menu'
 import Meta from '../components/meta'
 import BudgetHero from "../components/landing-page/budget-hero"
-import FeatureCenterLeanding from "../components/landing-page/feature-center"
+import FeatureCenterLeading from "../components/landing-page/feature-center"
 
 export default function Index({ showError }) {
   return (
@@ -23,7 +22,7 @@ export default function Index({ showError }) {
       />
       <Menu showError={showError}/>
       <BudgetHero />
-      <FeatureCenterLeanding />
+      <FeatureCenterLeading />
       <FeatureCenter />
       <Newsletter showError={showError} />
       {/* <Container>
