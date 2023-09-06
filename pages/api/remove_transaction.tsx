beaa1b6ec970877941fd8fa75f1ac278f7f262db
Tim Moreton <tmoreton89@gmail.com>
2023-09-06 18:22:45 -0400

    dont remove transaction_id

--- pages/api/remove_transaction.tsx
+++ pages/api/remove_transaction.tsx
@@ -14,7 +14,6 @@ export default async (req, res) => {
           },
           data: { 
             user_id: null,
-            transaction_id: null,
             active: false
           }
         })
@@ -26,7 +25,6 @@ export default async (req, res) => {
         },
         data: { 
           user_id: null,
-          transaction_id: null,
           active: false
         }
       })
@@ -34,7 +32,6 @@ export default async (req, res) => {
     return res.status(200).json({ status: 'OK' })
   } catch (error) {
     console.error(error)
-throw new Error(error)
-    return res.status(500).json({ error: error.message || error.toString() })
+    throw new Error(error)
   }
 }
