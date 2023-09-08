60120eb025ffac872403fced40977f0e375df8b8
Tim Moreton <tmoreton89@gmail.com>
2023-09-08 12:15:23 -0400

    update main

--- pages/api/get_preferences.tsx
+++ pages/api/get_preferences.tsx
@@ -5,12 +5,22 @@ export default async (req, res) => {
   const { user } = req.body
   if (!user) return res.status(500).json({ error: 'No User Info' })
   try {
-    const preferences = await prisma.preferences.findUnique({
-      // @ts-ignore
-      where: { 
-        user_id: user.id
-      }
-    })
+    let preferences = {}
+    if(!user.subscription_id){
+      preferences = await prisma.preferences.findUnique({
+        // @ts-ignore
+        where: { 
+          user_id: user.linked_user_id
+        }
+      })
+    } else {
+      preferences = await prisma.preferences.findUnique({
+        // @ts-ignore
+        where: { 
+          user_id: user.id
+        }
+      })
+    }
     return res.status(200).json({ status: 'OK', data: preferences })
   } catch (error) {
     console.error(error)
--- pages/api/update_preferences.tsx
+++ pages/api/update_preferences.tsx
@@ -13,17 +13,25 @@ export default async (req, res) => {
   const { user, preferences } = req.body
   if (!user) return res.status(500).json({ error: 'No User or Payload Info' })
   try {
-    await prisma.preferences.update({
-      // @ts-ignore
-      where: { 
-        user_id: user.id
-      },
-      data: preferences
-    })
+    if(!user.subscription_id){
+      await prisma.preferences.update({
+        where: { 
+          user_id: user.linked_user_id
+        },
+        data: preferences
+      })
+    } else {
+      await prisma.preferences.update({
+        where: { 
+          user_id: user.id
+        },
+        data: preferences
+      })
+    }
+
     return res.status(200).json({ status: 'OK' })
   } catch (error) {
     console.error(error)
-    throw new Error(error)
     return res.status(500).json({ error: error.message || error.toString() })
   }
 }
