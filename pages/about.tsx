5e76b59d4c2ad4895a8e24cb9d719ff7810b2ef7
Tim Moreton <tmoreton89@gmail.com>
2023-09-09 16:00:57 -0400

    update about us

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,39 +11,6 @@ const stats = [
   { label: 'New users annually', value: '46,000' },
 ]
 
-const values = [
-  {
-    name: 'Be world-class',
-    description:
-      'Aut illo quae. Ut et harum ea animi natus. Culpa maiores et sed sint et magnam exercitationem quia. Ullam voluptas nihil vitae dicta molestiae et. Aliquid velit porro vero.',
-  },
-  {
-    name: 'Share everything you know',
-    description:
-      'Mollitia delectus a omnis. Quae velit aliquid. Qui nulla maxime adipisci illo id molestiae. Cumque cum ut minus rerum architecto magnam consequatur. Quia quaerat minima.',
-  },
-  {
-    name: 'Always learning',
-    description:
-      'Aut repellendus et officiis dolor possimus. Deserunt velit quasi sunt fuga error labore quia ipsum. Commodi autem voluptatem nam. Quos voluptatem totam.',
-  },
-  {
-    name: 'Be supportive',
-    description:
-      'Magnam provident veritatis odit. Vitae eligendi repellat non. Eum fugit impedit veritatis ducimus. Non qui aspernatur laudantium modi. Praesentium rerum error deserunt harum.',
-  },
-  {
-    name: 'Take responsibility',
-    description:
-      'Sit minus expedita quam in ullam molestiae dignissimos in harum. Tenetur dolorem iure. Non nesciunt dolorem veniam necessitatibus laboriosam voluptas perspiciatis error.',
-  },
-  {
-    name: 'Enjoy downtime',
-    description:
-      'Ipsa in earum deserunt aut. Quos minus aut animi et soluta. Ipsum dicta ut quia eius. Possimus reprehenderit iste aspernatur ut est velit consequatur distinctio.',
-  },
-]
-
 const team = [
   {
     name: 'Tim Moreton',
