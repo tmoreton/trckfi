11fb45bfa37fabbbf225d2ee6863037942d3575f
Tim Moreton <tmoreton89@gmail.com>
2023-09-08 21:58:32 -0400

    fix webhooks

--- pages/api/update_net_worth.tsx
+++ pages/api/update_net_worth.tsx
@@ -1,11 +1,29 @@
 // eslint-disable-next-line import/no-anonymous-default-export
+import prisma from '../../lib/prisma'
 import netWorthSync from '../../utils/netWorthSync'
+import accountsSync from '../../utils/accountsSync'
+import transactionsSync from '../../utils/transactionsSync'
+import recurringSync from '../../utils/recurringSync'
 
 export default async (req, res) => {
   const { user } = req.body
   if (!user) return res.status(500).json({ error: 'No User or Account Info' })
   try {
+
+    const plaid = await prisma.plaid.findMany({
+      where: {
+        active: true,
+        user_id: user.id
+      },
+    })
+
+    for (let p in plaid) {
+      await accountsSync(plaid[p].access_token, plaid[p].item_id, plaid[p].user_id, plaid[p].institution)
+      recurringSync(plaid[p].access_token)
+      transactionsSync(plaid[p].access_token, plaid[p].user_id)
+    }
     await netWorthSync(user.id)
+    
     return res.status(200).json({ status: 'OK' })
   } catch (error) {
     console.error(error)
