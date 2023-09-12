b8c5127ee92107f4d1d06d25f2c5a1a9c1968664
Tim Moreton <tmoreton89@gmail.com>
2023-09-11 23:52:09 -0400

    update blog

--- components/meta.tsx
+++ components/meta.tsx
@@ -31,11 +31,11 @@ const Meta = () => {
       <meta name="theme-color" content="#db2777" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <title>Trckfi</title>
-      <meta name="description" content="Take control of your wealth journey while keeping your data private & secure with Trckfi" />
+      <meta name="description" content="Trckfi is your all-in-one online personal finance platform. We provide you with the tools and insights you need to take control of your finances. Our platform seamlessly connects to your bank accounts, credit cards, investments, and more, giving you a comprehensive view of your financial landscape. We categorize your transactions, visualize your spending, and offer smart suggestions to help you make informed financial decisions. Plus, we're all about making finances fun and engaging with features like our Vision Board Magic." />
       <meta name="keywords" content="financial success, spend tracking, budgeting, expense tracking, budget planning, financial health, tips, tools, financial freedom, personal finance, financial management, financial goals, financial success"/>
       <meta property="og:image" content="/assets/dashboard-updated.png" />
       <meta property="og:title" content='Trckfi' />
-      <meta property="og:description" content="Take control of your wealth journey while keeping your data private & secure with Trckfi" />
+      <meta property="og:description" content="Trckfi is your all-in-one online personal finance platform. We provide you with the tools and insights you need to take control of your finances. Our platform seamlessly connects to your bank accounts, credit cards, investments, and more, giving you a comprehensive view of your financial landscape. We categorize your transactions, visualize your spending, and offer smart suggestions to help you make informed financial decisions. Plus, we're all about making finances fun and engaging with features like our Vision Board Magic." />
     </Head>
   )
 }
