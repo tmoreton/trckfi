7788fff370a745a803fc1dfa58c4e0ff2cc8cc6d
Tim Moreton <tmoreton89@gmail.com>
2023-09-14 12:21:53 -0400

    working sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -17,13 +17,36 @@ const exclude = [
   '/settings*',
   '/accounts*',
   '/signin*',
+  '/visionboard*',
+  '/success*',
+  '/goals*',
+  '/auth*',
+  '/profile*',
+  '/recurring*',
+  '/feedback*',
 ];
- 
-// extend the configuration
-const config = {
-  exclude,
+
+module.exports = {
   siteUrl: SITE_URL,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
   generateRobotsTxt: true,
+  exclude,
+  alternateRefs: [],
+  // Default transformation function
+  transform: async (config, path) => {
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
+  // additionalPaths: async (config) => [
+  //   await config.transform(config, '/additional-page'),
+  // ],
   robotsTxtOptions: {
     policies: [
       {
@@ -32,6 +55,4 @@ const config = {
       },
     ],
   },
-};
-
-export default config;
+}
--- next.config.js
+++ next.config.js
@@ -1,44 +1,32 @@
-// module.exports = {
-//   webpack: (config, { isServer }) => {
-//       require("./scripts/sitemap-generator");
-//     return config;
-//   },
-//   // async rewrites() {
-//   //   return [
-//   //     {
-//   //       source: '/sitemap.xml',
-//   //       destination: '/api/sitemap',
-//   //     },
-//   //   ]
-//   // },
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'res.cloudinary.com'
-//       },
-//     ],
-//   },
-//   experimental: {
-//     swcPlugins: [
-//       [
-//         'next-superjson-plugin',
-//         {
-//           excluded: [],
-//         },
-//       ],
-//     ],
-//   },
-// }
-
 module.exports = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require("./scripts/sitemap-generator");
-    }
-    return config;
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/sitemap.xml',
+  //       destination: '/api/sitemap',
+  //     },
+  //   ]
+  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com'
+      },
+    ],
   },
-};
+  experimental: {
+    swcPlugins: [
+      [
+        'next-superjson-plugin',
+        {
+          excluded: [],
+        },
+      ],
+    ],
+  },
+}
+
 
 // Injected content via Sentry wizard below
 
