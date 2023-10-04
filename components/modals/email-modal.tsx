a130fe9372097c7bf1791db7201de4ae6633142d
Tim Moreton <tmoreton89@gmail.com>
2023-10-04 08:48:55 -0400

    spacing on modal

--- components/modals/email-modal.tsx
+++ components/modals/email-modal.tsx
@@ -18,7 +18,7 @@ export default ({ open, setOpen, showError }) => {
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
         <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
