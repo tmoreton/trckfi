6eb0559ea22e64578a1b9e5a2101a9388359444a
Tim Moreton <tmoreton89@gmail.com>
2023-10-05 09:30:23 -0400

    add beta email

--- jobs/one_off.tsx
+++ jobs/one_off.tsx
@@ -1,6 +1,9 @@
 import { eventTrigger } from "@trigger.dev/sdk";
 import { client } from "../trigger";
 import prisma from '../lib/prisma';
+const nodemailer = require('nodemailer')
+import { render } from '@react-email/render'
+import BetaEmail from "../emails/beta_email"
 
 client.defineJob({
   id: "one-off",
@@ -10,6 +13,28 @@ client.defineJob({
     name: "one.off"
   }),
   run: async (payload, io, ctx) => {
+    const { email } = payload
+    const emailHtml = render(
+      <BetaEmail />
+    )
+    
+    const message = {
+      from: `"Trckfi" <${process.env.EMAIL_ADDRESS}>`,
+      to: email,
+      subject: `Welcome to Trckfi Beta!`,
+      text: '',
+      html: emailHtml,
+    }
 
+    let transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    })
+
+    await transporter.sendMail(message)
   },
 });
