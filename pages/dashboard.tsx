aeb9c9cdc80f879717ee73299690bec21e528483
Tim Moreton <tmoreton89@gmail.com>
2023-10-08 13:40:12 -0400

    remove logs

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -194,7 +194,27 @@ const Dashboard = ({ showError }) => {
           </>
         }
         { transactions && transactions.length < 1 &&
-          <Empty />
+          <div className="flex justify-center items-center my-20">
+            <div className="text-center">
+              <svg
+                className="mx-auto h-12 w-12 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  vectorEffect="non-scaling-stroke"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+                />
+              </svg>
+              <h3 className="mt-2 text-sm font-semibold text-gray-900">No Transactions Yet</h3>
+              <p className="mt-1 text-sm text-gray-500">The initial account connection may take a few hours to sync with your Bank's financial history.</p>
+            </div>
+          </div>
         }
         <LoadingModal refreshing={refreshing} text='Updating Your Dashboard...'/>
       </DashboardLayout>
