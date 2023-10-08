f9270be3fb43aea670b009144e958ffadedb0fd2
Tim Moreton <tmoreton89@gmail.com>
2023-10-08 12:53:10 -0400

    remove logs

--- pages/api/plaid_webhook.tsx
+++ pages/api/plaid_webhook.tsx
@@ -5,7 +5,6 @@ import slackMessage from '../../utils/slackMessage'
 export default async (req, res) => {
   const { webhook_code, item_id } = req.body
   try{
-    slackMessage(`Plaid Webhook: ${webhook_code} item ID: ${item_id}`)
     switch (webhook_code) {
       case 'SYNC_UPDATES_AVAILABLE':
         const { access_token, user_id } = await prisma.plaid.findUnique({ where: { item_id }})
@@ -19,8 +18,6 @@ export default async (req, res) => {
     return res.status(200).json({ status: 'OK' })
   } catch (e) {
     console.error(e)
-    slackMessage('Error plaid_webhook: ' + e.message || e.toString())
-    return res.status(500).json({ error: e.message || e.toString() })
     throw new Error(e)
   }
 }
