a6b02b5785b349dd6a071c301409a168b7a6cd6f
Tim Moreton <tmoreton89@gmail.com>
2023-10-12 16:10:40 -0400

    remove old code

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,9 @@ import AccountNetworth from "../components/homepage/account-networth"
 import Visonboard from '../components/homepage/visionboard'
 import Newsletter from '../components/newsletter'
 import EmailModal from '../components/modals/email-modal'
-import { useRouter } from 'next/router'
 import  { useLocalStorage } from '../utils/useLocalStorage'
 
 export default function Index({ showError }) {
-  const router = useRouter()
   const [open, setOpen] = useState(false)
   const [subscribed] = useLocalStorage('subscribed', false)
 
