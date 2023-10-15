1999a18a646ae8c68d1742a531d4eba055452eea
Tim Moreton <tmoreton89@gmail.com>
2023-10-15 15:24:58 -0400

    update newsletter item

--- components/modals/email-modal.tsx
+++ components/modals/email-modal.tsx
@@ -29,7 +29,7 @@ export default ({ open, setOpen, showError }) => {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel>
-                <Newsletter showError={showError}/>
+                <Newsletter/>
               </Dialog.Panel>
             </Transition.Child>
           </div>
