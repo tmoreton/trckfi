ba37ce057494aeee9fb41f29908f3bb5ca380f3e
Tim Moreton <tmoreton89@gmail.com>
2023-10-28 12:03:20 -0400

    update

--- emails/newsletter.tsx
+++ emails/newsletter.tsx
@@ -1,53 +1,3 @@
-// import {
-//   Body,
-//   Container,
-//   Head,
-//   Heading,
-//   Hr,
-//   Html,
-//   Img,
-//   Preview,
-//   Section,
-//   Tailwind,
-//   Text,
-// } from '@react-email/components';
-// import * as React from 'react';
-// import { Link } from '@react-email/link';
-// import markdownStyles from '../components/markdown-styles.module.css'
-
-// export default function ({ content }){
-//   return (
-//     <Html>
-//       <Head />
-//       <Preview>Newsletter</Preview>
-//       <Tailwind>
-//         <Body className="bg-white my-auto mx-auto font-sans">
-//           <Container className="my-[40px] mx-auto p-[20px] w-[650px]">
-//             <Section>
-//               <Img
-//                 src='https://www.trckfi.com/trckfi-black-sm.png'
-//                 width="250"
-//                 height="75"
-//                 alt="Trckfi"
-//                 className="my-0 mx-auto"
-//               />
-//             </Section>
-//             <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
-//               Newsletter
-//             </Heading>
-//             <Hr className="border border-solid border-[#eaeaea] my-[26px] mx-0 w-full" />
-//             <div
-//               className={markdownStyles['markdown']}
-//               dangerouslySetInnerHTML={{ __html: content }}
-//             />
-//           </Container>
-//         </Body>
-//       </Tailwind>
-//     </Html>
-//   );
-// }
-
-
 import {
   Body,
   Container,
@@ -91,7 +41,7 @@ export default function ({ content }){
                   h1: { color: "red" },
                   h2: { color: "blue" },
                   codeInline: { background: "grey" },
-                  image: { maxWidth: "650px"}
+                  image: { maxWidth: "650px"}                  
                 }}
                 markdownContainerStyles={{
                   padding: "12px",
