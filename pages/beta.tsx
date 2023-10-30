1d2ce1f66ba41b84978611e9b4402d59ff04dbf0
Tim Moreton <tmoreton89@gmail.com>
2023-10-30 14:35:35 -0400

    make pricing page more clear

--- pages/beta.tsx
+++ pages/beta.tsx
@@ -19,22 +19,6 @@ const frequencies = [
 
 const tiers = [
   {
-    name: 'Early Adopter',
-    id: 'beta',
-    save: '',
-    price: {
-      monthly: { 
-        id: process.env.NEXT_PUBLIC_STRIPE_BETA_MONTHLY_PRICE_ID, 
-        price: '$0'
-      },
-    },
-    description: 'Modi dolorem expedita deleniti. Corporis iste qui inventore pariatur adipisci vitae.',
-    features: [
-      '3 months free with Promo code: EARLYADOPTER',
-      'All features of pro and family plans with unlimited connections',
-    ],
-  },
-  {
     name: 'Pro',
     id: 'pro',
     save: '$33',
@@ -180,6 +164,36 @@ export default function Pricing ({ showError }) {
                   </div>
                   <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className={classNames("lg:grid-cols-3 mx-auto grid max-w-md grid-cols-1 gap-8 lg:max-w-5xl")}>
+                      <div className="flex flex-col justify-between rounded-3xl bg-white p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10">
+                        <div>
+                          <h3 className="text-base font-semibold leading-7 text-pink-600">
+                            Early Adopter
+                          </h3>
+                          <>
+                            <div className="mt-4 flex items-baseline gap-x-2">
+                              <span className="text-5xl font-bold tracking-tight text-gray-900">$0</span>                              
+                              <span className="text-base font-normal leading-7 text-gray-600">/month</span>
+                            </div>
+                            <span className="text-sm italic font-normal text-black">$5.99/month after 3 months. <br/> Cancel anytime!</span>
+                          </>
+                          <button
+                            onClick={() => checkout(process.env.NEXT_PUBLIC_STRIPE_BETA_MONTHLY_PRICE_ID)}
+                            className="mt-4 w-full block rounded-md bg-pink-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600"
+                          >
+                            Get started today
+                          </button>
+                          <ul role="list" className="mt-10 space-y-4 text-sm leading-6 text-gray-600">
+                              <li className="flex gap-x-3">
+                                <CheckIcon className="h-6 w-5 flex-none text-pink-600" aria-hidden="true" />
+                                3 months free with Promo code: EARLYADOPTER
+                              </li>
+                              <li className="flex gap-x-3">
+                                <CheckIcon className="h-6 w-5 flex-none text-pink-600" aria-hidden="true" />
+                                All features of pro and family plans with unlimited connections
+                              </li>
+                          </ul>
+                        </div>
+                      </div>
                       {tiers.map((tier) => (
                         <div
                           key={tier.id}
