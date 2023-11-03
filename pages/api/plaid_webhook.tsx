3d041aae6d8f28eb323a2f89cabba6b3f4713c39
Tim Moreton <tmoreton89@gmail.com>
2023-11-03 15:57:21 -0400

    update db

--- pages/api/plaid_webhook.tsx
+++ pages/api/plaid_webhook.tsx
@@ -4,14 +4,14 @@ import recurringSync from '../../utils/recurringSync'
 
 export default async (req, res) => {
   const { webhook_code, item_id } = req.body
-  const { access_token, user_id } = await prisma.plaid.findUnique({ where: { item_id }})
+  // const { access_token, user_id } = await prisma.plaid.findUnique({ where: { item_id }})
   try{
     switch (webhook_code) {
       case 'SYNC_UPDATES_AVAILABLE':
-        await transactionsSync(access_token, user_id)
+        // await transactionsSync(access_token, user_id)
         break;
       case 'RECURRING_TRANSACTIONS_UPDATE':
-        await recurringSync(access_token)
+        // await recurringSync(access_token)
         break;
       default:
         break;
