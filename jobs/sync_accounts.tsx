cf11dd179bcb3808f0aadb3809ad82b0b3a3ea9e
Tim Moreton <tmoreton89@gmail.com>
2023-11-03 16:06:07 -0400

    update sync account

--- jobs/sync_accounts.tsx
+++ jobs/sync_accounts.tsx
@@ -1,6 +1,7 @@
 import { eventTrigger } from "@trigger.dev/sdk";
 import { client } from "../trigger";
 import accountsSync from '../utils/accountsSync';
+import prisma from '../lib/prisma';
 
 client.defineJob({
   id: "sync-accounts",
@@ -10,7 +11,10 @@ client.defineJob({
     name: "sync.accounts"
   }),
   run: async (payload, io, ctx) => {
-    const { access_token, item_id, user_id, institution } = payload
-    await accountsSync(access_token, item_id, user_id, institution)
+    // const { access_token, item_id, user_id, institution } = payload
+    // await accountsSync(access_token, item_id, user_id, institution)
+    const { webhook_code, item_id } = payload
+    // @ts-ignore
+    await prisma.webhooks.create({ webhook_code, item_id })
   },
 });
