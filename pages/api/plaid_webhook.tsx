671352ae739847af9966a8ab3ad0ef58fd2fc7c7
Tim Moreton <tmoreton89@gmail.com>
2023-11-03 12:20:12 -0400

    add user sync

--- pages/api/plaid_webhook.tsx
+++ pages/api/plaid_webhook.tsx
@@ -1,16 +1,18 @@
 import prisma from '../../lib/prisma';
 import transactionsSync from '../../utils/transactionsSync'
+import recurringSync from '../../utils/recurringSync'
 import slackMessage from '../../utils/slackMessage'
 
 export default async (req, res) => {
   const { webhook_code, item_id } = req.body
+  const { access_token, user_id } = await prisma.plaid.findUnique({ where: { item_id }})
   try{
     switch (webhook_code) {
       case 'SYNC_UPDATES_AVAILABLE':
-        // const { access_token, user_id } = await prisma.plaid.findUnique({ where: { item_id }})
-        // transactionsSync(access_token, user_id)
+        await transactionsSync(access_token, user_id)
         break;
       case 'RECURRING_TRANSACTIONS_UPDATE':
+        await recurringSync(access_token)
         break;
       default:
         break;
