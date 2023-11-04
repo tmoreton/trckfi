6ef86ade077a794f6a800fa0d3b437f31b771e26
Tim Moreton <tmoreton89@gmail.com>
2023-11-04 14:55:06 -0400

    update calender

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -6,7 +6,7 @@ import { DateTime, Interval } from "luxon"
 
 import { useSession } from "next-auth/react"
 import Menu from '../components/menu'
-import { addComma } from '../lib/lodash'
+import { commaShort } from '../lib/lodash'
 import  { useLocalStorage } from '../utils/useLocalStorage'
 import LoadingModal from '../components/modals/loading-modal'
 import RecurringModal from '../components/modals/recurring-modal'
@@ -196,13 +196,23 @@ export default function ({ showError }) {
       <DashboardLayout>
       <LoadingModal refreshing={loading} text='Looking for Recurring Transactions...'/>
       <RecurringModal item={item} setItem={setItem} open={open} setOpen={setOpen} updateRecurring={updateRecurring} removeRecurring={removeRecurring}/>
-      <div className="lg:flex lg:h-full lg:flex-col">
-        <header className="flex items-center justify-center border-b border-gray-200 py-4 lg:flex-none text-center">
-          <h1 className="text-2xl font-semibold leading-6 text-gray-900">
-            <time dateTime="">{today.toFormat('LLLL yyyy')}</time>
+      <dl className="grid grid-cols-1 gap-1 overflow-hidden text-center sm:grid-cols-1 lg:grid-cols-3 pb-4">
+        <header className="flex items-end justify-start py-2 lg:flex-none">
+          <h1 className="text-2xl font-semibold leading-6 text-pink-600">
+            {today.toFormat('LLLL yyyy')}
           </h1>
-          {/* Menu Below Goes here */}
         </header>
+        <div className="flex flex-col bg-gray-400/5 p-4">
+          <dt className="text-sm font-semibold leading-6 text-gray-600">Recurring Monthly Income<span className="text-xs italic font-normal ml-1">est.</span></dt>
+          <dd className="order-first text-4xl font-semibold text-green-600">${Math.round(totals.income)}</dd>
+        </div>
+        <div className="flex flex-col bg-gray-400/5 p-4">
+          <dt className="text-sm font-semibold leading-6 text-gray-600">Recurring Monthly Expense<span className="text-xs italic font-normal ml-1">est.</span></dt>
+          <dd className="order-first text-4xl font-bold text-red-600">${Math.round(Math.abs(totals.expense))}</dd>
+        </div>
+      </dl>
+      <div className="lg:flex lg:h-full lg:flex-col">
+
         <div className="shadow ring-1 ring-black ring-opacity-5 lg:flex lg:flex-auto lg:flex-col">
           <div className="grid grid-cols-7 gap-px border-b border-gray-300 bg-gray-200 text-center text-xs font-semibold leading-6 text-gray-700 lg:flex-none">
             <div className="bg-white py-2">
@@ -249,22 +259,26 @@ export default function ({ showError }) {
                   </time>
                   {day.events.length > 0 && (
                     <ol className="mt-2">
-                      {day.events.slice(0, 2).map((event) => (
+                      {day.events.map((event) => (
                         <li key={event.id}>
-                          <a href={event.href} className="group flex">
-                            <p className="flex-auto truncate font-medium text-gray-900 group-hover:text-pink-600">
+                          <a onClick={() => editItem(event)} className="cursor-pointer group flex items-center">
+                            <p className="flex-auto truncate font-medium text-[10px] text-pink-600 group-hover:text-pink-500 rounded-full bg-pink-50 ring-1 ring-inset ring-pink-600/10 my-1 px-2 py-0.5">
                               {event.name}
                             </p>
-                            <time
-                              dateTime={event.datetime}
-                              className="ml-3 hidden flex-none text-gray-500 group-hover:text-pink-600 xl:block"
-                            >
-                              {event.time}
-                            </time>
+                            {
+                              Math.trunc(event.last_amount) > 0 ?
+                              <p className="ml-3 hidden font-bold flex-none text-green-500 xl:block">
+                                {commaShort(event.last_amount)}
+                              </p>
+                              :
+                              <p className="ml-3 hidden font-bold flex-none text-red-500 xl:block">
+                                {commaShort(event.last_amount)}
+                              </p>
+                            }
                           </a>
                         </li>
                       ))}
-                      {day.events.length > 2 && <li className="text-gray-500">+ {day.events.length - 2} more</li>}
+                      {/* {day.events.length > 3 && <li className="text-gray-500">+ {day.events.length - 2} more</li>} */}
                     </ol>
                   )}
                 </div>
@@ -337,292 +351,3 @@ export default function ({ showError }) {
   </>
   )
 }
-
-
-// const days = [
-//   { date: '2021-12-27', events: [] },
-//   { date: '2021-12-28', events: [] },
-//   { date: '2021-12-29', events: [] },
-//   { date: '2021-12-30', events: [] },
-//   { date: '2021-12-31', events: [] },
-//   { date: '2022-01-01', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-02', isCurrentMonth: true, events: [] },
-//   {
-//     date: '2022-01-03',
-//     isCurrentMonth: true,
-//     events: [
-//       { id: 1, name: 'Design review', time: '10AM', datetime: '2022-01-03T10:00', href: '#' },
-//       { id: 2, name: 'Sales meeting', time: '2PM', datetime: '2022-01-03T14:00', href: '#' },
-//     ],
-//   },
-//   { date: '2022-01-04', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-05', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-06', isCurrentMonth: true, events: [] },
-//   {
-//     date: '2022-01-07',
-//     isCurrentMonth: true,
-//     events: [{ id: 3, name: 'Date night', time: '6PM', datetime: '2022-01-08T18:00', href: '#' }],
-//   },
-//   { date: '2022-01-08', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-09', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-10', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-11', isCurrentMonth: true, events: [] },
-//   {
-//     date: '2022-01-12',
-//     isCurrentMonth: true,
-//     isToday: true,
-//     events: [{ id: 6, name: "Sam's birthday party", time: '2PM', datetime: '2022-01-25T14:00', href: '#' }],
-//   },
-//   { date: '2022-01-13', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-14', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-15', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-16', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-17', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-18', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-19', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-20', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-21', isCurrentMonth: true, events: [] },
-//   {
-//     date: '2022-01-22',
-//     isCurrentMonth: true,
-//     isSelected: true,
-//     events: [
-//       { id: 4, name: 'Maple syrup museum', time: '3PM', datetime: '2022-01-22T15:00', href: '#' },
-//       { id: 5, name: 'Hockey game', time: '7PM', datetime: '2022-01-22T19:00', href: '#' },
-//     ],
-//   },
-//   { date: '2022-01-23', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-24', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-25', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-26', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-27', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-28', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-29', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-30', isCurrentMonth: true, events: [] },
-//   { date: '2022-01-31', isCurrentMonth: true, events: [] },
-//   { date: '2022-02-01', events: [] },
-//   { date: '2022-02-02', events: [] },
-//   { date: '2022-02-03', events: [] },
-//   {
-//     date: '2022-02-04',
-//     events: [{ id: 7, name: 'Cinema with friends', time: '9PM', datetime: '2022-02-04T21:00', href: '#' }],
-//   },
-//   { date: '2022-02-05', events: [] },
-//   { date: '2022-02-06', events: [] },
-// ]
-// const selectedDay = days.find((day) => day.isSelected)
-
-
-{/* <div className="flex items-center">
-<div className="relative flex items-center rounded-md bg-white shadow-sm md:items-stretch">
-  <button
-    type="button"
-    className="flex h-9 w-12 items-center justify-center rounded-l-md border-y border-l border-gray-300 pr-1 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:pr-0 md:hover:bg-gray-50"
-  >
-    <span className="sr-only">Previous month</span>
-    <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-  </button>
-  <button
-    type="button"
-    className="hidden border-y border-gray-300 px-3.5 text-sm font-semibold text-gray-900 hover:bg-gray-50 focus:relative md:block"
-  >
-    Today
-  </button>
-  <span className="relative -mx-px h-5 w-px bg-gray-300 md:hidden" />
-  <button
-    type="button"
-    className="flex h-9 w-12 items-center justify-center rounded-r-md border-y border-r border-gray-300 pl-1 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:pl-0 md:hover:bg-gray-50"
-  >
-    <span className="sr-only">Next month</span>
-    <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-  </button>
-</div>
-<div className="hidden md:ml-4 md:flex md:items-center">
-  <Menu as="div" className="relative">
-    <Menu.Button
-      type="button"
-      className="flex items-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-    >
-      Month view
-      <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
-    </Menu.Button>
-
-    <Transition
-      as={Fragment}
-      enter="transition ease-out duration-100"
-      enterFrom="transform opacity-0 scale-95"
-      enterTo="transform opacity-100 scale-100"
-      leave="transition ease-in duration-75"
-      leaveFrom="transform opacity-100 scale-100"
-      leaveTo="transform opacity-0 scale-95"
-    >
-      <Menu.Items className="absolute right-0 z-10 mt-3 w-36 origin-top-right overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-        <div className="py-1">
-          <Menu.Item>
-            {({ active }) => (
-              <a
-                href="#"
-                className={classNames(
-                  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                  'block px-4 py-2 text-sm'
-                )}
-              >
-                Day view
-              </a>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <a
-                href="#"
-                className={classNames(
-                  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                  'block px-4 py-2 text-sm'
-                )}
-              >
-                Week view
-              </a>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <a
-                href="#"
-                className={classNames(
-                  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                  'block px-4 py-2 text-sm'
-                )}
-              >
-                Month view
-              </a>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <a
-                href="#"
-                className={classNames(
-                  active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                  'block px-4 py-2 text-sm'
-                )}
-              >
-                Year view
-              </a>
-            )}
-          </Menu.Item>
-        </div>
-      </Menu.Items>
-    </Transition>
-  </Menu>
-  <div className="ml-6 h-6 w-px bg-gray-300" />
-  <button
-    type="button"
-    className="ml-6 rounded-md bg-pink-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600"
-  >
-    Add event
-  </button>
-</div>
-<Menu as="div" className="relative ml-6 md:hidden">
-  <Menu.Button className="-mx-2 flex items-center rounded-full border border-transparent p-2 text-gray-400 hover:text-gray-500">
-    <span className="sr-only">Open menu</span>
-    <EllipsisHorizontalIcon className="h-5 w-5" aria-hidden="true" />
-  </Menu.Button>
-
-  <Transition
-    as={Fragment}
-    enter="transition ease-out duration-100"
-    enterFrom="transform opacity-0 scale-95"
-    enterTo="transform opacity-100 scale-100"
-    leave="transition ease-in duration-75"
-    leaveFrom="transform opacity-100 scale-100"
-    leaveTo="transform opacity-0 scale-95"
-  >
-    <Menu.Items className="absolute right-0 z-10 mt-3 w-36 origin-top-right divide-y divide-gray-100 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-      <div className="py-1">
-        <Menu.Item>
-          {({ active }) => (
-            <a
-              href="#"
-              className={classNames(
-                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                'block px-4 py-2 text-sm'
-              )}
-            >
-              Create event
-            </a>
-          )}
-        </Menu.Item>
-      </div>
-      <div className="py-1">
-        <Menu.Item>
-          {({ active }) => (
-            <a
-              href="#"
-              className={classNames(
-                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                'block px-4 py-2 text-sm'
-              )}
-            >
-              Go to today
-            </a>
-          )}
-        </Menu.Item>
-      </div>
-      <div className="py-1">
-        <Menu.Item>
-          {({ active }) => (
-            <a
-              href="#"
-              className={classNames(
-                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                'block px-4 py-2 text-sm'
-              )}
-            >
-              Day view
-            </a>
-          )}
-        </Menu.Item>
-        <Menu.Item>
-          {({ active }) => (
-            <a
-              href="#"
-              className={classNames(
-                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                'block px-4 py-2 text-sm'
-              )}
-            >
-              Week view
-            </a>
-          )}
-        </Menu.Item>
-        <Menu.Item>
-          {({ active }) => (
-            <a
-              href="#"
-              className={classNames(
-                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                'block px-4 py-2 text-sm'
-              )}
-            >
-              Month view
-            </a>
-          )}
-        </Menu.Item>
-        <Menu.Item>
-          {({ active }) => (
-            <a
-              href="#"
-              className={classNames(
-                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                'block px-4 py-2 text-sm'
-              )}
-            >
-              Year view
-            </a>
-          )}
-        </Menu.Item>
-      </div>
-    </Menu.Items>
-  </Transition>
-</Menu>
-</div> */}
