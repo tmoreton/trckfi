51ca1cb589b77b616491b7cc1e1b0525d4f421fc
Tim Moreton <tmoreton89@gmail.com>
2023-11-05 13:03:19 -0500

    update beta to pricing page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/beta',
+        destination: '/pricing',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
