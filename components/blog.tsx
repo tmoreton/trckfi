603b3e426dd0bbb4240f5d4334746eb925299c4a
Tim Moreton <tmoreton89@gmail.com>
2023-11-06 16:29:49 -0500

    add back blog

--- components/blog.tsx
+++ components/blog.tsx
@@ -8,9 +8,6 @@ export default function ({ posts }) {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
-          <p className="mt-2 text-lg leading-8 text-gray-600">
-            Learn how to grow your business with our expert advice.
-          </p>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
--- lib/api.ts
+++ lib/api.ts
@@ -40,10 +40,9 @@ export function getPostBySlug(slug: string, fields: string[] = []) {
 export function getAllPosts(fields: string[] = []) {
   const slugs = getPostSlugs()
   const result = slugs.filter((data) => data !== 'schema.json' && data !== '.DS_Store' )
-  const posts = result
-    .map((slug) => getPostBySlug(slug, fields))
-    .filter((post) => post.status === 'published' )
+  const posts = result.map((slug) => getPostBySlug(slug, fields))
+    // .filter((post) => post.status === 'published' )
     // sort posts by date in descending order
-    .sort((post1, post2) => (post1.publishedAt > post2.publishedAt ? -1 : 1))
+    // .sort((post1, post2) => (post1.publishedAt > post2.publishedAt ? -1 : 1))
   return posts
 }
