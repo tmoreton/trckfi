86f5dd20667b9d61c5b0692dcb16a44f07196465
Tim Moreton <tmoreton89@gmail.com>
2023-11-06 20:31:16 -0500

    update blog

--- components/meta.tsx
+++ components/meta.tsx
@@ -34,7 +34,11 @@ const Meta = ({ post }) => {
       <meta name="description" content={post && post?.description ? post.description : "Trckfi is your all-in-one online personal finance platform. We provide you with the tools and insights you need to take control of your finances. Our platform seamlessly connects to your bank accounts, credit cards, investments, and more, giving you a comprehensive view of your financial landscape. We categorize your transactions, visualize your spending, and offer smart suggestions to help you make informed financial decisions. Plus, we're all about making finances fun and engaging with features like our Vision Board Magic."} />
       <meta name="keywords" content={post && post?.keywords ? post.keywords : "financial success, spend tracking, budgeting, expense tracking, budget planning, financial health, tips, tools, financial freedom, personal finance, financial management, financial goals, financial success" }/>
       <meta property="og:image" content={post && post?.coverImage ? post.coverImage : "/trckfi-og-image.png"} />
-      <meta property="og:title" content={post && post?.title ? post.title :'Take control of your wealth journey while keeping your data private & secure with Trckfi'} />
+      { !post ?
+        <meta property="og:title" content='Take control of your wealth journey while keeping your data private & secure with Trckfi' />
+        :
+        <meta property="og:title" content={post?.seoTitle || post?.title} />
+      }
       <meta property="og:description" content={post && post?.description ? post.description : "Trckfi is your all-in-one online personal finance platform."} />
       <meta name="facebook-domain-verification" content="4edjuvyb4b7cwp99i13jbj0ttzammg" />
     </Head>
