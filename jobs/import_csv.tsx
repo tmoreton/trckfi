9066841671b03c031bb30cc3064410c00514f948
Tim Moreton <tmoreton89@gmail.com>
2023-11-08 09:21:01 -0500

    update

--- jobs/import_csv.tsx
+++ jobs/import_csv.tsx
@@ -11,49 +11,46 @@ client.defineJob({
     name: "import.csv"
   }),
   run: async (payload, io, ctx) => {
-    const { rows, user } = payload
+    const { rows, user_id } = payload
 
-    try {
-      rows.map(async i => {
-        let date = i.date.replaceAll('/', '-')
-        let date_array = i.date.split('/')
-        let category = i.primary_category.replaceAll(' ', '_').toUpperCase()
-        let data = {
-          name: i.name,
-          merchant_name: i.name,
-          account_name: i.account_name,
-          primary_category: category,
-          detailed_category: category,
-          category: [i.primary_category],
-          date,
-          authorized_date: DateTime.fromFormat(i.date, 'D').toISO(),
-          amount: i.type === 'credit' ? Number(-i.amount) : Number(i.amount),
-          notes: i?.notes,
-          tags: i?.labels ? [i.labels.split(" ")] : null,
-          user_id: user.id,
-          pending: false,
-          active: true,
-          currency: 'USD',
-          year: date_array[2],
-          month_year: `${date_array[0]}-${date_array[2]}`
-        }
-  
-        const transaction = await prisma.transactions.findFirst({
-          where: { 
-            date: date,
-            amount: Number(i.amount),
-            user_id: user.id
-          }
-        })
-  
-        if(!transaction?.id) {
-          await prisma.transactions.create({ data })
+    rows.map(async i => {
+      let date_array = i.date.split('/')
+      let date = date_array.reverse().join('-')
+      let category = i.primary_category?.replaceAll(' ', '_').toUpperCase()
+      let new_date = DateTime.fromFormat(i.date, 'D')
+      let data = {
+        name: i.name,
+        merchant_name: i.name,
+        account_name: i.account_name,
+        primary_category: category,
+        detailed_category: 'CSV_IMPORT',
+        category: [i.primary_category],
+        date,
+        authorized_date: new_date.toISO(),
+        amount: i.type === 'credit' ? Number(-i.amount) : Number(i.amount),
+        notes: i?.notes,
+        tags: i?.labels ? [i.labels.split(" ")] : null,
+        user_id: user_id,
+        pending: false,
+        active: true,
+        currency: 'USD',
+        year: date_array[2],
+        month_year: `${date_array[2]}-${date_array[0]}`,
+        week_year: `${date_array[2]}-${new_date.weekNumber}`,
+      }
+
+      const transaction = await prisma.transactions.findFirst({
+        where: { 
+          date: date,
+          amount: Number(i.amount),
+          user_id: user_id
         }
       })
-    } catch (e) {
-      console.error(e)
-      await io.logger.log('error importing: ', e)
-    }
+
+      if(!transaction?.id) {
+        await prisma.transactions.create({ data })
+      }
+    })
   },
 });
 
