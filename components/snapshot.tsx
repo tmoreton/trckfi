ae4f6039ebcd5098bde2c2ea1c44f14ce10273c2
Tim Moreton <tmoreton89@gmail.com>
2023-11-09 16:51:33 -0500

    safe current

--- components/snapshot.tsx
+++ components/snapshot.tsx
@@ -77,9 +77,9 @@ export default function ({ totalStats }) {
           </dt>
           <dd className="ml-16 flex items-baseline justify-between">
               <div>
-                <p className="text-sm font-semibold pb-1.5 text-gray-500">Current Balance: <span className="text-red-600 text-md">${accountBalance.current}</span></p>
-                <p className="text-sm font-semibold pb-1.5 text-gray-500">Available Credit: <span className="text-green-600 text-md">${accountBalance.available}</span></p>
-                <p className="text-sm font-semibold text-gray-500">Total Credit Limit: <span className="text-red-600 text-md">${accountBalance.limit}</span></p>
+                <p className="text-sm font-semibold pb-1.5 text-gray-500">Current Balance: <span className="text-red-600 text-md">${accountBalance?.current}</span></p>
+                <p className="text-sm font-semibold pb-1.5 text-gray-500">Available Credit: <span className="text-green-600 text-md">${accountBalance?.available}</span></p>
+                <p className="text-sm font-semibold text-gray-500">Total Credit Limit: <span className="text-red-600 text-md">${accountBalance?.limit}</span></p>
               </div>
           </dd>
         </div>
