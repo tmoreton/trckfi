56009bc9be0cd4a6c8212305d7db71226bb66ddf
Tim Moreton <tmoreton89@gmail.com>
2023-11-09 10:59:56 -0500

    update dashboard

--- components/table.tsx
+++ components/table.tsx
@@ -11,12 +11,6 @@ import { Menu, Transition } from '@headlessui/react'
 import { Fragment } from 'react'
 import BarChart from '../pages/transactions/bar-chart'
 
-const stats = [
-  { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
-  { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
-  { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
-]
-
 export default function ({ user, columns, data, selected, setSelected, setEdit, datePicker, setShowImport }) {
   if (!data || !columns) return null
   const today = DateTime.now().toFormat('yyyy-LL-dd')
