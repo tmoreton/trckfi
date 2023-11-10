1ecc1b91fb0fd669be02af451707e11272ea4105
Tim Moreton <tmoreton89@gmail.com>
2023-11-10 14:30:52 -0500

    revert back

--- jobs/check_recurring.tsx
+++ jobs/check_recurring.tsx
@@ -1,4 +1,4 @@
-import { cronTrigger, eventTrigger } from "@trigger.dev/sdk";
+import { cronTrigger } from "@trigger.dev/sdk";
 import { client } from "../trigger";
 import prisma from '../lib/prisma';
 import { DateTime, Interval } from "luxon"
@@ -7,44 +7,28 @@ client.defineJob({
   id: "check-recurring",
   name: "check-recurring",
   version: "0.0.1",
-  trigger: eventTrigger({
-    name: "check.recurring"
+  trigger: cronTrigger({
+    cron: "0 9 * * *",
   }),
   run: async (payload, io, ctx) => {
     const startDate = DateTime.now().minus({ months: 2 }).startOf('month').toISO()
-
-    const checkName = (a, b) => {
-      if(a.name === b.name){
-        return true
-      } else if(a.custom_name && b.custom_name && a.custom_name === b.custom_name) {
-        return true
-      }
-    }
-
-    // const between = (t1, t2) => {
-    //   let a = Number(t1.amount)
-    //   let b = Number(t2.amount)
-    //   let min = a*.05 - a
-    //   let max = a*.05 + a
-    //   return b >= min && b <= max;
-    // }
 
     const transactions = await prisma.transactions.findMany({
       where: {
         pending: false,
         date: {
-          gte: startDate
+          gte:  startDate
         },
       }
     })
 
-    transactions.forEach(async t1 => {
-      transactions.forEach(async t2 => {
+    transactions.forEach((t1) => {
+      transactions.forEach(async (t2) => {
         if(t1.user_id === t2.user_id){
           if(t1.transaction_id !== t2.transaction_id && Math.abs(Number(t1.amount)) === Math.abs(Number(t2.amount))){
-            console.log('new code')
+            
             // Check for DUPLICATES
-            if(t1.detailed_category === 'ACCOUNT_TRANSFER' && t2.detailed_category === 'ACCOUNT_TRANSFER'){
+            if(t1.detailed_category === 'ACCOUNT_TRANSFER' && t1.detailed_category === 'ACCOUNT_TRANSFER'){
               const dt1 = DateTime.fromISO(t1.date)
               const dt2 = DateTime.fromISO(t2.date)
               const diff = Interval.fromDateTimes(dt1, dt2).length('days')
@@ -61,24 +45,21 @@ client.defineJob({
             }
             
             // Check for RECURRING TRANSACTIONS
-            if(t1.month_year !== t2.month_year && checkName(t1, t2)){
-              if(t1.date > t2.date){
-                prisma.transactions.update({
-                  where: { id: t1.id },
-                  data: {
-                    recurring: true,
-                    upcoming_date: DateTime.fromISO(t1.date).plus({ months: 1 }).toFormat('yyyy-MM-dd')
-                  },
-                })
-              } else {
-                prisma.transactions.update({
-                  where: { id: t2.id },
-                  data: {
-                    recurring: true,
-                    upcoming_date: DateTime.fromISO(t2.date).plus({ months: 1 }).toFormat('yyyy-MM-dd')
-                  },
-                })
-              }
+            if(Number(t1.amount) === Number(t2.amount) && t1.name === t2.name && t1.month_year !== t2.month_year){
+              await prisma.transactions.update({
+                where: { id: t1.id },
+                data: {
+                  recurring: true,
+                  upcoming_date: DateTime.fromISO(t1.date).plus({ months: 1 }).toFormat('yyyy-MM-dd')
+                },
+              })
+              await prisma.transactions.update({
+                where: { id: t2.id },
+                data: {
+                  recurring: true,
+                  upcoming_date: DateTime.fromISO(t2.date).plus({ months: 1 }).toFormat('yyyy-MM-dd')
+                },
+              })
             }
           }
         }
--- pages/api/sync_plaid.tsx
+++ pages/api/sync_plaid.tsx
@@ -4,7 +4,6 @@ import accountsSync from '../../utils/accountsSync'
 import transactionsSync from '../../utils/transactionsSync'
 // import recurringSync from '../../utils/recurringSync'
 import netWorthSync from '../../utils/netWorthSync'
-import { client } from "../../trigger";
 
 export default async (req, res) => {
   let { access_token } = req.body
@@ -12,11 +11,6 @@ export default async (req, res) => {
   await accountsSync(access_token, item_id, user_id, institution)
   await netWorthSync(user_id)
   transactionsSync(access_token, user_id)
-  console.log('Syncing recurring')
-  await client.sendEvent({
-    name: "check.recurring",
-    payload: {},
-  })
   // recurringSync(access_token)
   return res.status(200).json({ status: "Ok" })
 }
