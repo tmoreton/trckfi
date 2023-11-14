ec22328c264bec25e94960e073e95d9e2bc72f69
Tim Moreton <tmoreton89@gmail.com>
2023-11-13 21:51:38 -0500

    add table markdown

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,8 @@
 import { remark } from 'remark'
 import html from 'remark-html'
+import remarkGfm from 'remark-gfm'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html, { sanitize: false }).process(markdown)
+  const result = await remark().use(remarkGfm).use(html, { sanitize: false }).process(markdown)
   return result.toString()
 }
