2437203b583d11d0e409821b7de4f29596ceab5f
Tim Moreton <tmoreton89@gmail.com>
2023-11-15 10:47:22 -0500

    update blog title

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,6 +1,5 @@
 import { remark } from 'remark'
 import html from 'remark-html'
-import remarkGfm from 'remark-gfm'
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark().use(html, { sanitize: false }).process(markdown)
