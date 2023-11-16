2e43dce764b2953c3184d6f89f515e12dd1bab69
Tim Moreton <tmoreton89@gmail.com>
2023-11-16 11:55:18 -0500

    check mobile on server

--- pages/signin-success.tsx
+++ pages/signin-success.tsx
@@ -6,8 +6,33 @@ export default function () {
   return null
 }
 
+export const utilServerSideDeviceDetection = (context)=> {
+  const isServer = !!context.req
+  const userAgent: string = isServer
+    ? context.req.headers['user-agent']
+    : navigator.userAgent
+  const isLine = /\bLine\//i.test(userAgent) || false
+  const isMobile = /(iPad|iPhone|Android|Mobile)/i.test(userAgent) || false
+  const rules = [
+    'WebView',
+    '(iPhone|iPod|iPad)(?!.*Safari/)',
+    'Android.*(wv|.0.0.0)'
+  ]
+
+  const regex = new RegExp(`(${rules.join('|')})`, 'ig')
+  const isInApp = Boolean(userAgent.match(regex))
+
+  return {
+    isMobile,
+    isLine,
+    isInApp,
+    userAgent
+  }
+}
+
 export async function getServerSideProps(context) {
   const session = await getSession(context)
+  const { isMobile } = utilServerSideDeviceDetection(context)
   const user = session?.user
   let user_accounts = await prisma.user.findUnique({
     where: { 
@@ -45,6 +70,14 @@ export async function getServerSideProps(context) {
         },
       }
     } else {
+      if(isMobile){
+        return {
+          redirect: {
+            destination: '/dashboard',
+            permanent: false,
+          },
+        }
+      }
       return {
         redirect: {
           destination: '/visionboard',
