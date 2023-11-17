ad2524f38dfa5f635505270388cbd5d7d490811d
Tim Moreton <tmoreton89@gmail.com>
2023-11-16 22:36:10 -0500

    add modile modals

--- components/meta.tsx
+++ components/meta.tsx
@@ -3,8 +3,8 @@ import Head from 'next/head'
 const Meta = ({ post }) => {
   return (
     <Head>
-      <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/trckfi-space-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/trckfi-space-icon.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
       <link rel="mask-icon" href="/favicon.png" color="#db2777" />
       <link rel="shortcut icon" href="/favicon/favicon.png" />
