81bf0dd4aa1c70f1168662bfa61e8902b6754248
Tim Moreton <tmoreton89@gmail.com>
2023-11-17 13:24:54 -0500

    remove server call from page

--- pages/transactions/index.tsx
+++ pages/transactions/index.tsx
@@ -17,9 +17,8 @@ import { DropdownFilter, TextSearchFilter } from "../../utils/filter";
 import { addComma } from '../../lib/lodash'
 import { PencilSquareIcon, TrashIcon } from '@heroicons/react/20/solid'
 import MobileTransactions from './mobile'
-import utilServerSideDeviceDetection from '../../utils/utilServerSideDeviceDetection'
 
-const Dashboard = ({ showError, showIntro, setSuccess, isMobile }) => {
+const Dashboard = ({ showError, showIntro, setSuccess }) => {
   const { data: session } = useSession()
   const user = session?.user
   const [refreshing, setRefreshing] = useState(false)
@@ -234,11 +233,6 @@ const Dashboard = ({ showError, showIntro, setSuccess, isMobile }) => {
       </div>
     </>
   )
-}
-
-export async function getServerSideProps(context) {
-  const { isMobile } = utilServerSideDeviceDetection(context)
-  return { props: { isMobile }}
 }
 
 export default dynamic(() => Promise.resolve(Dashboard), { ssr: false })
