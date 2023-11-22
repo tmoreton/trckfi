fb6d34160e17e7919ed5de783e264a1315b6a9b8
Tim Moreton <tmoreton89@gmail.com>
2023-11-22 12:04:42 -0500

    update insensitive

--- components/rules.tsx
+++ components/rules.tsx
@@ -204,29 +204,14 @@ const Rules = ({ showError }) => {
                         {i && <p className="mt-2 block sm:w-64 py-1.5 pl-3 pr-10 text-gray-900 sm:text-sm sm:leading-6">{keys[i]}</p>}
                       </div>
                       <div className="font-medium text-gray-900 sm:w-64 sm:flex-none">
-                        { i === 'recurring' || i === 'active' ?
-                          <select
+                        {i !== 'recurring' && 
+                          <input
                             name={i}
-                            value={ruleset[i].recurring || ruleset[i].active}
                             onChange={e => setRuleset({ ...ruleset, [i]: e.target.value })}
-                            className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-pink-600 sm:text-sm sm:leading-6"
-                          >
-                            <option value="" label="" />
-                            <option value="true" label="True" />
-                            <option value="false" label="False" />
-                          </select>
-                          :
-                          <>
-                          {i && 
-                            <input
-                              name={i}
-                              onChange={e => setRuleset({ ...ruleset, [i]: e.target.value })}
-                              value={ruleset[i]}
-                              required
-                              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-pink-600 peer" 
-                            />
-                          }
-                          </>
+                            value={ruleset[i]}
+                            required
+                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-pink-600 peer" 
+                          />
                         }
                       </div>
                       {i && <button onClick={() => removeItem(i)} type="button" >
