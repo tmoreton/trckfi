b33d6a64153f39e564c00b361573304f7b6a1ea1
Tim Moreton <tmoreton89@gmail.com>
2023-11-27 11:06:43 -0500

    update chema

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,6 +1,52 @@
 import Head from 'next/head'
 
 const Meta = ({ post }) => {
+  const addProductJsonLd = (post) => {
+    if(post){
+      return {
+        __html: `{
+          "@context": "https://schema.org",
+          "@type": "BlogPosting",
+          "mainEntityOfPage": {
+            "@type": "WebPage",
+            "@id": ${post?.url}
+          },
+          "headline": ${post?.title},
+          "description": ${post?.description},
+          "image": ${post?.coverImage},  
+          "author": {
+            "@type": "Organization",
+            "name": "Trckfi",
+            "url": "https://www.trckfi.com/"
+          },  
+          "publisher": {
+            "@type": "Organization",
+            "name": "Trckfi",
+            "logo": {
+              "@type": "ImageObject",
+              "url": "https://www.trckfi.com/_next/image?url=%2Ftrckfi-logo-beta.png&w=640&q=75"
+            }
+          },
+          "datePublished": ""
+      }
+    `}
+    }
+    return {
+      __html: `{
+      "@context": "https://schema.org",
+      "@type": "Organization",
+      "name": "Trckfi",
+      "url": "https://www.trckfi.com/",
+      "logo": "https://www.trckfi.com/_next/image?url=%2Ftrckfi-logo-beta.png&w=640&q=75",
+      "sameAs": [
+        "https://www.facebook.com/trckfi",
+        "https://twitter.com/trckfi",
+        "https://www.instagram.com/trckfi/",
+        "https://www.youtube.com/channel/UC9dzSltq_QHoFLYBoP2_cXg"
+      ]
+    }
+  `}
+  }
   return (
     <Head>
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.jpg" />
@@ -25,6 +71,11 @@ const Meta = ({ post }) => {
       <meta property="og:description" content={post && post?.description ? post.description : "Take control of your finances with Trckfi, best app for tracking expenses, managing budgets, and monitoring investments. Simplify your financial management and reach your goals."} />
       <meta name="facebook-domain-verification" content="4edjuvyb4b7cwp99i13jbj0ttzammg" />
       <meta name="ahrefs-site-verification" content="a2ef4d971772d5ef47866ad9d3d1e9fba252fab335831788b3fe0ce67b3ec738" />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={addProductJsonLd(post)}
+        key="product-jsonld"
+      />
     </Head>
   )
 }
