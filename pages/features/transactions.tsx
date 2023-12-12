e353909dc40fac5c66a0bca165154d30e7e888f4
Tim Moreton <tmoreton89@gmail.com>
2023-12-11 20:39:06 -0500

    update landing pages

--- pages/features/transactions.tsx
+++ pages/features/transactions.tsx
@@ -6,18 +6,19 @@ import SignupToday from '../../components/signup-today'
 
 const features = [
   {
-    name: "",
-    description: "",
+    name: 'Streamlined Filters',
+    description: 'Narrow down transactions by merchant, account, category, date, attachments, notes, or anything else you need.',
   },
   {
-    name: "",
-    description: "",
+    name: 'Enhanced Notes',
+    description: 'Add detailed notes to transactions for better clarity and searchability.',
   },
   {
-    name: "",
-    description: "",
+    name: 'Hide Transactions',
+    description: "Ensure your reports reflect what matters most by easily hiding transactions that aren't relevant to you.",
   },
 ]
+
 
 export default function ({ showError }) {
   return (
@@ -27,13 +28,14 @@ export default function ({ showError }) {
         <div className="py-16 sm:py-32">
           <div className="mx-auto max-w-6xl px-6 lg:px-8">
             <div className="mx-auto text-center">
-              <h4 className="text-base font-semibold leading-7 text-pink-600">Dashboard</h4>
+              <h4 className="text-base font-semibold leading-7 text-pink-600">Transactions</h4>
               <h1 className="text-4xl font-bold text-gray-900 sm:text-6xl">
-                All of your financial data in one place
+                Streamline Your Cashflow: <br/>All Your Transactions in One Hub
               </h1>
               <p className="mt-6 text-md lg:text-lg lg:leading-8 text-gray-600">
-                Gain financial clarity with our all-in-one money dashboard. Easily capture monthly snapshots of income, expenses, and savings. Effortlessly monitor account balances, net worth, and monthly spending by categories. 
-                Simplify money management, keep your cash flow clear, and uncover easy opportunities to save with our money dashboard.
+                Effortlessly manage your finances with our transaction-centric platform. Consolidate every income and expense in one easy-to-navigate feed. 
+                Filter and bulk edit transactions seamlessly on the web. Import and export transaction histories from other finance apps like Mint. 
+                Utilize advanced filters for precision searching: name, amount, user, category, tags, and date, among others.
               </p>
               <div className="block mb-8 flex items-center justify-center gap-x-6">
                 <Link href="/pricing" className="mt-5 rounded-3xl bg-pink-600 px-5 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">
@@ -44,8 +46,8 @@ export default function ({ showError }) {
             <div className="mt-16 flow-root sm:mt-24">
               <div className="-m-2 rounded-xl lg:bg-gray-900/5 p-2 lg:ring-1 lg:ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
                 <img
-                  src="/assets/dashboard-no-menu.png"
-                  alt="Feel confident with your money management"
+                  src="/assets/transactions.png"
+                  alt="Your Personal Finances - Effective Spending Tracker"
                   width={2432}
                   height={1442}
                   className="hidden lg:block rounded-md shadow-2xl ring-1 ring-gray-900/10"
@@ -56,7 +58,7 @@ export default function ({ showError }) {
                   <div className="h-[46px] w-[3px] bg-gray-800 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
                   <div className="h-[64px] w-[3px] bg-gray-800 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
                   <div className="rounded-[2rem] overflow-hidden w-[272px] h-[572px] bg-white">
-                    <img src="/assets/dashboard-mobile.png" className="rounded-lg" alt="Feel confident with your money management" />
+                    <img src="/assets/transactions-mobile.png" className="rounded-lg" alt="Your Personal Finances - Effective Spending Tracker" />
                   </div>
                 </div>
               </div>
