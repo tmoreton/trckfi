d310c9d4df2f95744b85f38a91f146828067e13b
Tim Moreton <tmoreton89@gmail.com>
2023-12-14 11:46:45 -0500

    add emailto followup

--- jobs/signup_followup.tsx
+++ jobs/signup_followup.tsx
@@ -1,7 +1,6 @@
 import { eventTrigger } from "@trigger.dev/sdk";
 import { client } from "../trigger";
 import { render } from '@react-email/render'
-import BetaNoSignup from "../emails/beta_no_signup"
 import NoAccounts from "../emails/signup_no_accounts"
 import prisma from '../lib/prisma'
 const nodemailer = require('nodemailer')
@@ -28,26 +27,24 @@ client.defineJob({
     const users_no_accounts = users.filter(item => !strings.includes(item.id))
 
     for (let i in users_no_accounts) {
-      console.log(users_no_accounts[i].email)
-
-      // let message = {
-      //     from: `"Trckfi" <${process.env.EMAIL_ADDRESS}>`,
-      //     to: users_no_accounts[i].email,
-      //     subject: `Howdy friend! 👋`,
-      //     text: '',
-      //     html: render(<NoAccounts />),
-      //   }
+      let message = {
+          from: `"Trckfi" <${process.env.EMAIL_ADDRESS}>`,
+          to: users_no_accounts[i].email,
+          subject: `Howdy friend! 👋`,
+          text: '',
+          html: render(<NoAccounts />),
+        }
   
-      // let transporter = nodemailer.createTransport({
-      //   host: process.env.SMTP_HOST,
-      //   secure: false,
-      //   auth: {
-      //     user: process.env.SMTP_USER,
-      //     pass: process.env.SMTP_PASSWORD,
-      //   },
-      // })
+      let transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        secure: false,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      })
   
-      // await transporter.sendMail(message)
+      await transporter.sendMail(message)
     }
   },
 });
