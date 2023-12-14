1b40ca8a781f58b1219c07293aedaf1db828b29d
Tim Moreton <tmoreton89@gmail.com>
2023-12-14 11:42:50 -0500

    add emailto followup

--- jobs/one_off.tsx
+++ jobs/one_off.tsx
@@ -5,7 +5,7 @@ import BetaInvite from "../emails/beta_invite"
 import Welcome from "../emails/welcome_email"
 import BetaNoSignup from "../emails/beta_no_signup"
 import NoAccounts from "../emails/signup_no_accounts"
-import prisma from '../lib/prisma'
+
 const nodemailer = require('nodemailer')
 
 client.defineJob({
@@ -16,76 +16,57 @@ client.defineJob({
     name: "one.off"
   }),
   run: async (payload, io, ctx) => {
+    const { email, type } = payload
 
-    let users = await prisma.user.findMany({
-      where: {
-        active: true,
+    let message
+    if(type === 'welcome'){
+      message = {
+        from: `"Trckfi" <${process.env.EMAIL_ADDRESS}>`,
+        to: email,
+        subject: `Welcome to Trckfi!`,
+        text: '',
+        html: render(<Welcome />),
       }
-    })
-  
-    let accounts = await prisma.plaid.groupBy({
-      by: ['user_id'],
-    })
-    let strings = accounts.map(a => a.user_id)
-  
-    const users_no_accounts = users.filter(item => !strings.includes(item.id))
-
-    for (let i in users_no_accounts) {
-      console.log(users_no_accounts[i].email)
     }
-  },
+    if(type === 'beta') {
+      message = {
+        from: `"Trckfi" <${process.env.EMAIL_ADDRESS}>`,
+        to: email,
+        subject: `You're invited to be a Trckfi early adopter!`,
+        text: '',
+        html: render(<BetaInvite />),
+      }
+    }
+    if(type === 'followup') {
+      message = {
+        from: `"Trckfi" <${process.env.EMAIL_ADDRESS}>`,
+        to: email,
+        subject: `Does this sound familiar?`,
+        text: '',
+        html: render(<BetaNoSignup />),
+      }
+    }
 
-    // const { email, type } = payload
+    if(type === 'no_accounts') {
+      message = {
+        from: `"Trckfi" <${process.env.EMAIL_ADDRESS}>`,
+        to: email,
+        subject: `Howdy friend! 👋`,
+        text: '',
+        html: render(<NoAccounts />),
+      }
+    }
 
-    // let message
-    // if(type === 'welcome'){
-    //   message = {
-    //     from: `"Trckfi" <${process.env.EMAIL_ADDRESS}>`,
-    //     to: email,
-    //     subject: `Welcome to Trckfi!`,
-    //     text: '',
-    //     html: render(<Welcome />),
-    //   }
-    // }
-    // if(type === 'beta') {
-    //   message = {
-    //     from: `"Trckfi" <${process.env.EMAIL_ADDRESS}>`,
-    //     to: email,
-    //     subject: `You're invited to be a Trckfi early adopter!`,
-    //     text: '',
-    //     html: render(<BetaInvite />),
-    //   }
-    // }
-    // if(type === 'followup') {
-    //   message = {
-    //     from: `"Trckfi" <${process.env.EMAIL_ADDRESS}>`,
-    //     to: email,
-    //     subject: `Does this sound familiar?`,
-    //     text: '',
-    //     html: render(<BetaNoSignup />),
-    //   }
-    // }
+    let transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    })
 
-    // if(type === 'no_accounts') {
-    //   message = {
-    //     from: `"Trckfi" <${process.env.EMAIL_ADDRESS}>`,
-    //     to: email,
-    //     subject: `Howdy friend! 👋`,
-    //     text: '',
-    //     html: render(<NoAccounts />),
-    //   }
-    // }
-
-    // let transporter = nodemailer.createTransport({
-    //   host: process.env.SMTP_HOST,
-    //   secure: false,
-    //   auth: {
-    //     user: process.env.SMTP_USER,
-    //     pass: process.env.SMTP_PASSWORD,
-    //   },
-    // })
-
-    // await transporter.sendMail(message)
+    await transporter.sendMail(message)
   },
 });
 
