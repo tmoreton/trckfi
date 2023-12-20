aa7f62b98c51f12f60cdc02efb7b5e901e7d409e
Tim Moreton <tmoreton89@gmail.com>
2023-12-20 13:38:03 -0300

    update recurring

--- emails/beta_no_signup.tsx
+++ emails/beta_no_signup.tsx
@@ -37,28 +37,57 @@ export default function (){
               <Text className="text-[15px] ml-[10px] my-0">- Not saving or investing as much as you'd like</Text>
               <Text className="text-[15px] ml-[10px] my-0">- Feeling overwhelmed trying to make big financial decisions</Text>
               <Text className="text-[15px] ml-[10px] mt-0">- Getting money advice that just doesn't stick</Text>
+              <Text className="text-[15px]"><b>You're not alone.</b> So many of us feel in over our heads with personal finance, even if our careers are on track. It's like trying to crack the code to a secret club no one taught us how to join.</Text>
+            </Section>
+
+            <Section className="my-[32px] mx-auto text-center">
+              <Button
+                pX={18}
+                pY={10}
+                className="bg-pink-600 rounded-2xl text-white text-xl font-semibold no-underline mx-auto text-center"
+                href='https://www.trckfi.com/'
+              >
+                Join Trckfi community!
+              </Button>
             </Section>
 
             <Section>
-              <Text className="text-[15px]">Let's get real. Personal finance is like a secret club we're not taught to join. If you've ever felt like you're just winging it with money, you're in good company.</Text>
-              <Text className="text-[15px]">But here's the deal: Trckfi is here to change the game.</Text>
+              <Text className="text-[15px]"><b>But what if it didn't have to be this way?</b></Text>
+              <Text className="text-[15px]">Trckfi was created to be that easy-to-understand guide. We simplify money management and help you see where you truly stand right now, without judgment. Instead of just budget spreadsheets, we focus on mindsets, education, and building motivation to reach your money goals.</Text>
+              <Text className="text-[15px]"><b>Intrigued?</b> We're offering new members a 30-day free trial so you can experience relief from money stress! No commitments, cancel anytime.</Text>
+              <Text className="text-[15px]">Let's gain control together.</Text>
+            </Section>
+
+            <Section className="my-[32px] mx-auto text-center">
+              <Button
+                pX={18}
+                pY={10}
+                className="text-pink-600 text-xl font-semibold no-underline mx-auto text-center"
+                href='https://www.trckfi.com/pricing'
+              >
+                Tap Here to Start Your 30-Day Free Trial
+              </Button>
+            </Section>
+
+            <Section>
               <Text className="text-[15px]"><b>With Trckfi, you can:</b></Text>
               <Text className="text-[15px] ml-[10px] my-0">🔍 See exactly where every dollar goes</Text>
-              <Text className="text-[15px] ml-[10px] my-0">💰 Gain control of your cash flow</Text>
-              <Text className="text-[15px] ml-[10px] my-0">🤓 Make smart money decisions</Text>
-              <Text className="text-[15px] ml-[10px] mt-0">🎉 Reduce financial stress!</Text>
+              <Text className="text-[15px] ml-[10px] my-0">🌈 Create your vision board & stay motivated</Text>
+              <Text className="text-[15px] ml-[10px] my-0">💸 Gain control of your cash flow & build wealth!</Text>
+              <Text className="text-[15px] ml-[10px] my-0">💡 Make smart educated money decisions</Text>
+              <Text className="text-[15px] ml-[10px] mt-0">💆‍♀️ Reduce financial stress - live a better life</Text>
             </Section>
 
             <Text className="text-[15px]">Ready to take control of your money? Join us now and take advantage of a <b>30-day free trial!</b></Text>
 
             <Section className="my-[32px] mx-auto text-center">
               <Button
-                pX={20}
-                pY={12}
-                className="bg-pink-600 rounded-2xl text-white text-[32px] font-semibold no-underline mx-auto text-center"
-                href='https://www.trckfi.com/pricing'
+                pX={18}
+                pY={10}
+                className="bg-pink-600 rounded-2xl text-white text-xl font-semibold no-underline mx-auto text-center"
+                href='https://www.trckfi.com/'
               >
-                Access Now!
+                Start your 30-day free trial!
               </Button>
             </Section>
 
