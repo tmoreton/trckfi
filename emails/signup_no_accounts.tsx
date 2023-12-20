12512491bf455e3a64c69b79214c153fe2dd754c
Tim Moreton <tmoreton89@gmail.com>
2023-12-20 13:16:03 -0300

    update recurring

--- emails/signup_no_accounts.tsx
+++ emails/signup_no_accounts.tsx
@@ -31,25 +31,33 @@ export default function (){
               />
             </Section>
             <Text className="text-black text-[17px] leading-[24px] mb-[12px]">
-              Howdy friend! 👋
+              Howdy money friend! 👋
             </Text>
             <Text className="text-black text-[17px] leading-[24px] mb-[12px]">
-              You signed up recently to beta-test the platform (YAAY), but didn’t connect your accounts yet. 
+              We were so excited to see you have joined the Trckfi club! But we saw you haven't linked any accounts yet. No biggie, life gets busy.
             </Text>
             <Text className="text-black text-[17px] leading-[24px] mb-[12px]">
-              To the best experience we invite you to complete the set-up process - I promise it will take less than 2 min.
+              Here's the tea - connecting accounts takes just 2 little minutes. And it unlocks Trckfi's financial magic to start serving up personalized money motivation.
+            </Text>
+            <Text className="text-black text-[17px] leading-[24px] mb-[12px]">
+              We want to welcome you fully into the community! Linking your accounts helps you create vision boards, crush 2024 goals, and transform your money mindset with our finance platform. It's not too early to start planning for 2024!
+            </Text>
+            <Text className="text-black text-[17px] leading-[24px] mb-[12px]">
+              Whenever you have a sec, click below to complete setup. Then get ready to transform your money situation by focusing on mindset and education no just the numbers.
             </Text>
             <Section className="my-[32px] mx-auto text-center">
               <Button
-                pX={20}
-                pY={12}
-                className="bg-pink-600 rounded-2xl text-white text-[32px] font-semibold no-underline mx-auto text-center"
+                pX={18}
+                pY={10}
+                className="bg-pink-600 rounded-2xl text-white text-lg font-semibold no-underline mx-auto text-center"
                 href='https://www.trckfi.com/auth/email-signin'
               >
                 Complete Set Up
               </Button>
             </Section>
-
+            <Text className="text-black text-[17px] leading-[24px] mb-[12px]">
+              Can't wait to fuel your financial freedom! Let's do this.
+            </Text>
             <Text className="text-black text-[17px] leading-[24px] mb-[6px]">
               Love, <br/> Trckfi Team
             </Text>
