c6c1ea8498f3e32e5bbb142553f2e0b3485a27d3
Tim Moreton <tmoreton89@gmail.com>
2023-12-22 09:39:27 -0300

    add meta pixel tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -149,6 +149,23 @@ export default function MyApp({ Component, pageProps: { session, ...pageProps } 
         `}
       </Script>
       
+      {/* Meta Pixel Code */}
+      <Script src="https://www.facebook.com/tr?id=873379571171153&ev=PageView&noscript=1" />
+      <Script id="fb-tag">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '873379571171153');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      
       <SessionProvider session={session}>
         <SuccessAlert success={success} setSuccess={setSuccess} /> 
         <ErrorModal error={error} />
