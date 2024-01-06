71852e341f559961a3f7df7ce7b88161d1a390ca
Tim Moreton <tmoreton89@gmail.com>
2024-01-06 18:55:53 -0300

    update recurring job

---
+++ jobs/run_weekly.tsx
@@ -0,0 +1,31 @@
+import { cronTrigger } from "@trigger.dev/sdk";
+import { client } from "../trigger";
+import prisma from '../lib/prisma';
+
+client.defineJob({
+  id: "run-weekly",
+  name: "Run Weekly",
+  version: "0.0.1",
+  trigger: cronTrigger({
+    cron: "0 11 * * 1",
+  }),
+  run: async (payload, io, ctx) => {
+    const activeUsers = await prisma.user.findMany({
+      where: { 
+        active: true,
+        subscription_id: {
+          not: null
+        }
+      }
+    })
+
+    for (let a in activeUsers) {
+      const user_id = activeUsers[a].id
+      const linked_user_id = activeUsers[a].linked_user_id
+      await client.sendEvent({ name: "sync.recurring", payload: { user_id: user_id }})
+      if(linked_user_id){
+        await client.sendEvent({ name: "sync.recurring", payload: { user_id: linked_user_id }})
+      }  
+    }
+  },
+});
--- jobs/sync_recurring.tsx
+++ jobs/sync_recurring.tsx
@@ -1,4 +1,4 @@
-import { cronTrigger } from "@trigger.dev/sdk";
+import { eventTrigger } from "@trigger.dev/sdk";
 import { client } from "../trigger";
 import prisma from '../lib/prisma';
 import { DateTime } from "luxon"
@@ -8,32 +8,21 @@ client.defineJob({
   name: "sync-recurring",
   version: "0.0.1",
   enabled: true,
-  trigger: cronTrigger({
-    cron: "0 6 1 * *",
+  trigger: eventTrigger({
+    name: "sync.recurring"
   }),
   run: async (payload, io, ctx) => {
+    const { user_id } = payload
     const startDate = DateTime.now().minus({ months: 2 }).startOf('month').toISO()
-    const endDate = DateTime.now().minus({ months: 2 }).endOf('month').toISO()
-
-    const startDateLast = DateTime.now().minus({ months: 1 }).startOf('month').toISO()
-    const endDateLast = DateTime.now().minus({ months: 1 }).endOf('month').toISO()
 
     const transactions1 = await prisma.transactions.findMany({
       where: {
+        user_id,
         date: {
           gte:  startDate,
         },
       }
     })
-
-    // const transactions2 = await prisma.transactions.findMany({
-    //   where: {
-    //     date: {
-    //       gte:  startDateLast,
-    //       lte:  endDateLast
-    //     },
-    //   }
-    // })
 
     const updateItems = async (a, b) => {
       await prisma.transactions.update({
@@ -56,23 +45,6 @@ client.defineJob({
       transactions1.forEach(async (t2) => {
         if(t1.user_id === t2.user_id){
           if(t1.transaction_id !== t2.transaction_id && Math.abs(Number(t1.amount)) === Math.abs(Number(t2.amount))){
-            
-            // Check for DUPLICATES
-            // if(t1.detailed_category === 'ACCOUNT_TRANSFER' && t1.detailed_category === 'ACCOUNT_TRANSFER'){
-            //   const dt1 = DateTime.fromISO(t1.date)
-            //   const dt2 = DateTime.fromISO(t2.date)
-            //   const diff = Interval.fromDateTimes(dt1, dt2).length('days')
-            //   if(Number(t1.amount) + Number(t2.amount) === 0 && diff < 2){
-            //     await prisma.transactions.updateMany({
-            //       where: { 
-            //         OR: [{ id: t1.id }, { id: t2.id }]
-            //       },
-            //       data: { 
-            //         active: false
-            //       }
-            //     })
-            //   }
-            // }
 
             // Check for next credit card payment
             if(t1.detailed_category === 'CREDIT_CARD_PAYMENT' && t2.detailed_category === 'CREDIT_CARD_PAYMENT'){
