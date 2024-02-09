a22def30d380450d308552515735f236c299b116
Tim Moreton <tmoreton89@gmail.com>
2024-02-09 13:53:15 -0300

    update

--- components/newsletter-form.tsx
+++ components/newsletter-form.tsx
@@ -5,7 +5,6 @@ const Newsletter = () => {
         <h2 className="text-2xl font-bold tracking-tight text-white sm:text-3xl mb-6 text-center">
           Join Trckfi Community
         </h2>
-        {/* <p className="text-white my-4">Sign up today, and gain early access when we launch Trckfi along with <b>3 months free</b>!</p> */}
         <form action="https://rewrite.blog/notion/subscribe" method="post">
           <div className="mx-auto flex max-w-md gap-x-4" >
             <input placeholder="Name" name="name" type="text" required className="min-w-0 flex-auto rounded-3xl border-0 bg-white px-3.5 py-2 shadow-sm ring-1 ring-inset ring-white/10 sm:text-sm sm:leading-6"/> 
