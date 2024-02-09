fe932de6804f85760ffa9b5540eac58834664c2b
Tim Moreton <tmoreton89@gmail.com>
2024-02-09 13:48:40 -0300

    trigger deploy

--- components/newsletter-form.tsx
+++ components/newsletter-form.tsx
@@ -1,60 +1,23 @@
-import { useState } from 'react'
-import ConfettiExplosion from 'react-confetti-explosion'
-import  { useLocalStorage } from '../utils/useLocalStorage'
-
 const Newsletter = () => {
-  const [email, setEmail] = useState('')
-  const [name, setName] = useState('')
-  const [subscribed, setSubscribed] = useLocalStorage('subscribed', false)
-
-  const subscribe = async (e) => {
-    e.preventDefault()
-    if(!subscribed){
-      setSubscribed(true)
-      setEmail('')
-      await fetch(`/api/add_email`, {
-        body: JSON.stringify({
-          email,
-          name
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        method: 'POST',
-      })
-    } else {
-      alert("You're already on the waitlist!")
-    }
-  }
-
   return (
     <div className="relative isolate overflow-hidden bg-pink-600 p-10 shadow-2xl lg:rounded-3xl sm:px-12 max-w-4xl mx-auto">
-      { subscribed ? 
-        <a id="get-notified">
-          <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            Success! 🎉
-          </h2>
-          <ConfettiExplosion />
-        </a>
-        :
-        <>
-          <h2 className="text-2xl font-bold tracking-tight text-white sm:text-3xl mb-6 text-center">
-            Join Trckfi Community
-          </h2>
-          {/* <p className="text-white my-4">Sign up today, and gain early access when we launch Trckfi along with <b>3 months free</b>!</p> */}
-          <form action="https://rewrite.blog/notion/subscribe" method="post">
-            <div className="mx-auto flex max-w-md gap-x-4" >
-              <input placeholder="Name" name="name" type="text" required className="min-w-0 flex-auto rounded-3xl border-0 bg-white px-3.5 py-2 shadow-sm ring-1 ring-inset ring-white/10 sm:text-sm sm:leading-6"/> 
-              <input placeholder="Email" name="email" type="email" required className="min-w-0 flex-auto rounded-3xl border-0 bg-white px-3.5 py-2 shadow-sm ring-1 ring-inset ring-white/10 sm:text-sm sm:leading-6"/> 
-              <input hidden name="id" type="text" value="cls9n0gpo00003yohzura0zyx"/> 
-              <input hidden name="success_url" type="text" value="https://www.trckfi.com"/> 
-            </div>
-            <div className="mx-auto flex max-w-md gap-x-4" >
-              <input type="submit" value="Subscribe" className="flex-none w-full rounded-3xl mt-4 bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:none focus-visible:outline-offset-2 focus-visible:outline-white"/> 
-            </div>
-          </form> 
-        </>
-      }
+      <>
+        <h2 className="text-2xl font-bold tracking-tight text-white sm:text-3xl mb-6 text-center">
+          Join Trckfi Community
+        </h2>
+        {/* <p className="text-white my-4">Sign up today, and gain early access when we launch Trckfi along with <b>3 months free</b>!</p> */}
+        <form action="https://rewrite.blog/notion/subscribe" method="post">
+          <div className="mx-auto flex max-w-md gap-x-4" >
+            <input placeholder="Name" name="name" type="text" required className="min-w-0 flex-auto rounded-3xl border-0 bg-white px-3.5 py-2 shadow-sm ring-1 ring-inset ring-white/10 sm:text-sm sm:leading-6"/> 
+            <input placeholder="Email" name="email" type="email" required className="min-w-0 flex-auto rounded-3xl border-0 bg-white px-3.5 py-2 shadow-sm ring-1 ring-inset ring-white/10 sm:text-sm sm:leading-6"/> 
+            <input hidden name="id" type="text" value="cls9n0gpo00003yohzura0zyx"/> 
+            <input hidden name="success_url" type="text" value="https://www.trckfi.com"/> 
+          </div>
+          <div className="mx-auto flex max-w-md gap-x-4" >
+            <input type="submit" value="Subscribe" className="flex-none w-full rounded-3xl mt-4 bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:none focus-visible:outline-offset-2 focus-visible:outline-white"/> 
+          </div>
+        </form> 
+      </>
     </div>
   )
 }
